bd182dd5930bb933912bc79eb19485e0b586b698
Vishal Patil <believeinthelimitless@gmail.com>
2022-12-13 15:24:18 +0530

    Refactored MainActivity

--- app/src/main/java/com/example/gitagyan/MainActivity.kt
+++ app/src/main/java/com/example/gitagyan/MainActivity.kt
@@ -16,28 +16,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GitaGyanTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            GitaGyanApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GitaGyanApp() {
+    GitaGyanTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GitaGyanTheme {
-        Greeting("Android")
+        GitaGyanApp()
     }
 }
