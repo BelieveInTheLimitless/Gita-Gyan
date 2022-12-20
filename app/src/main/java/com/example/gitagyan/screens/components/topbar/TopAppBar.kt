eb0429e26b2a0f4709d517c1a1fe709cda2cecff
Vishal Patil <believeinthelimitless@gmail.com>
2022-12-20 21:15:58 +0530

    Added settings clickable in top bar

--- app/src/main/java/com/example/gitagyan/screens/components/topbar/TopAppBar.kt
+++ app/src/main/java/com/example/gitagyan/screens/components/topbar/TopAppBar.kt
@@ -1,12 +1,10 @@
 package com.example.gitagyan.screens.components.topbar
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
@@ -15,24 +13,36 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.gitagyan.screens.components.bottombar.BottomNavigation
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun TopAppBar(navController:NavController){
     Scaffold(
         topBar = {
-            androidx.compose.material.TopAppBar(backgroundColor = Color(0xFFFD950E)) {
-                Box(modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ){
+            TopAppBar(backgroundColor = Color(0xFFFD950E)) {
+                Surface(modifier = Modifier.fillMaxSize().padding(vertical = 13.dp),
+                color = Color(0xFFFD950E)) {
                     Text(text = "Gita Gyan",
                         color = Color.Black,
                         fontSize = 20.sp,
                         fontStyle = FontStyle.Italic,
                         fontWeight = FontWeight.Bold,
-                        fontFamily = FontFamily.Serif
+                        fontFamily = FontFamily.Serif,
+                        textAlign = TextAlign.Center
                     )
+                    Row(horizontalArrangement = Arrangement.End) {
+                        Icon(imageVector = Icons.Default.Settings,
+                            contentDescription = "Settings",
+                            modifier = Modifier
+                                .clickable { },
+                            tint = Color.Black)
+                    }
                 }
+
             }
         }
     ) {
