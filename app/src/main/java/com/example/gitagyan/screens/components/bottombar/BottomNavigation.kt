9f59ec726808bbbb3d088a95db9a9a2236707c12
Vishal Patil <believeinthelimitless@gmail.com>
2022-12-21 20:12:11 +0530

    Removed unnecessary scaffold

--- app/src/main/java/com/example/gitagyan/screens/components/bottombar/BottomNavigation.kt
+++ app/src/main/java/com/example/gitagyan/screens/components/bottombar/BottomNavigation.kt
@@ -1,7 +1,6 @@
 package com.example.gitagyan.screens.components.bottombar
 
 import android.annotation.SuppressLint
-import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
@@ -12,38 +11,33 @@ import com.example.gitagyan.navigation.AppScreens
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun BottomNavigation(navController: NavController){
-    Scaffold(
-        bottomBar = {
+    BottomNavigationItem(
+        items = listOf(
             BottomNavigationItem(
-                items = listOf(
-                    BottomNavigationItem(
-                        name = "Home",
-                        route = AppScreens.HomeScreen.name,
-                        icon = Icons.Default.Home
-                    ),
-                    BottomNavigationItem(
-                        name = "Search",
-                        route = AppScreens.SearchScreen.name,
-                        icon = Icons.Default.Search
-                    ),
-                    BottomNavigationItem(
-                        name = "Saved",
-                        route = AppScreens.SaveScreen.name,
-                        icon = Icons.Default.Favorite
-                    ),
-                    BottomNavigationItem(
-                        name = "Profile",
-                        route = AppScreens.ProfileScreen.name,
-                        icon = Icons.Default.Person
-                    )
-                ),
-                navController = navController,
-                onItemClick = {
-                    navController.navigate(it.route)
-                }
+                name = "Home",
+                route = AppScreens.HomeScreen.name,
+                icon = Icons.Default.Home
+            ),
+            BottomNavigationItem(
+                name = "Search",
+                route = AppScreens.SearchScreen.name,
+                icon = Icons.Default.Search
+            ),
+            BottomNavigationItem(
+                name = "Saved",
+                route = AppScreens.SaveScreen.name,
+                icon = Icons.Default.Favorite
+            ),
+            BottomNavigationItem(
+                name = "Profile",
+                route = AppScreens.ProfileScreen.name,
+                icon = Icons.Default.Person
             )
+        ),
+        navController = navController,
+        onItemClick = {
+            navController.navigate(it.route)
         }
-    ) {
-    }
+    )
 }
 
