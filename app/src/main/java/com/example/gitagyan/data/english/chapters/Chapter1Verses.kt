e1f3242c03f24382ef3fa8d52567753e278c4976
Vishal Patil <believeinthelimitless@gmail.com>
2023-01-02 14:06:04 +0530

    Added 1st english chapter

--- app/src/main/java/com/example/gitagyan/data/english/chapters/Chapter1Verses.kt
+++ app/src/main/java/com/example/gitagyan/data/english/chapters/Chapter1Verses.kt
@@ -2,13 +2,290 @@ package com.example.gitagyan.data.english.chapters
 
 import com.example.gitagyan.data.english.Verse
 
-fun chapter1Verses() : List<Verse>{
+fun getEnglishChapter1() : List<Verse>{
     return listOf(
         Verse(
             verse_id = 1,
             verse_name = "Verse 1",
-            verse = "धृतराष्ट्र उवाच\n\nधर्मक्षेत्रे कुरुक्षेत्रे समवेता युयुत्सवः।\nमामकाः पाण्डवाश्चैव किमकुर्वत सञ्जय।।1.1।।\n",
+            verse = "धृतराष्ट्र उवाच\n\nधर्मक्षेत्रे कुरुक्षेत्रे समवेता युयुत्सवः।\nमामकाः पाण्डवाश्चैव किमकुर्वत सञ्जय।।\n",
             verse_meaning = "Dhritarastra said: Sanjaya, gathered on the holy land of Kurukshetra, eager to fight, what did my sons and the sons of Pandu do?"
+        ),
+        Verse(
+            verse_id = 2,
+            verse_name = "Verse 2",
+            verse = "सञ्जय उवाच\n\nदृष्ट्वा तु पाण्डवानीकं व्यूढं दुर्योधनस्तदा।\nआचार्यमुपसङ्गम्य राजा वचनमब्रवीत्।।\n",
+            verse_meaning = "Sanjaya said: At that time, seeing the army of the Pandavas drawn up for battle and approaching Dronacharya, King Duryodhana spoke the following words :"
+        ),
+        Verse(
+            verse_id = 3,
+            verse_name = "Verse 3",
+            verse = "पश्यैतां पाण्डुपुत्राणामाचार्य महतीं चमूम्।\nव्यूढां द्रुपदपुत्रेण तव शिष्येण धीमता।।\n",
+            verse_meaning = "Behold, O Revered Master, the mighty army of the sons of Pandu arrayed for battle by your talented pupil, Dhrstadyumna, son of Drupada."
+        ),
+        Verse(
+            verse_id = 4,
+            verse_name = "Verse 4",
+            verse = "अत्र शूरा महेष्वासा भीमार्जुनसमा युधि।\nयुयुधानो विराटश्च द्रुपदश्च महारथः।।\n",
+            verse_meaning = "There are in this army, heroes wielding mighty bows and equal in military prowess to Bhima and Arjuna-Satyaki and Virata and the Maharatha (warrior chief) Drupada;"
+        ),
+        Verse(
+            verse_id = 5,
+            verse_name = "Verse 5",
+            verse = "धृष्टकेतुश्चेकितानः काशिराजश्च वीर्यवान्।\nपुरुजित्कुन्तिभोजश्च शैब्यश्च नरपुङ्गवः।।\n",
+            verse_meaning = "Dhrstaketu, Cekitana and the valiant King of Kasi, and Purujit, Kuntibhoja, and Saibya, the best of men,"
+        ),
+        Verse(
+            verse_id = 6,
+            verse_name = "Verse 6",
+            verse = "युधामन्युश्च विक्रान्त उत्तमौजाश्च वीर्यवान्।\nसौभद्रो द्रौपदेयाश्च सर्व एव महारथाः।।\n",
+            verse_meaning = "and mighty Yudhamanyu, and valiant Uttamauja, Abhimanyu, the son of Subhadra, and the five sons of Draupadi all of them Maharathis (warrior chiefs)."
+        ),
+        Verse(
+            verse_id = 7,
+            verse_name = "Verse 7",
+            verse = "अस्माकं तु विशिष्टा ये तान्निबोध द्विजोत्तम।\nनायका मम सैन्यस्य संज्ञार्थं तान्ब्रवीमि ते।।\n",
+            verse_meaning = "O best of Brahmanas, know them also who are the principal warriors on our side - the generals of my army. For your information I mention them."
+        ),
+        Verse(
+            verse_id = 8,
+            verse_name = "Verse 8",
+            verse = "भवान्भीष्मश्च कर्णश्च कृपश्च समितिञ्जयः।\nअश्वत्थामा विकर्णश्च सौमदत्तिस्तथैव च।।\n",
+            verse_meaning = "Yourself and Bhisma and Karna and Krpa, who is ever victorious in battle; and even so Asvatthama, Vikarna and Bhurisrava (the son of Somadatta);"
+        ),
+        Verse(
+            verse_id = 9,
+            verse_name = "Verse 9",
+            verse = "अन्ये च बहवः शूरा मदर्थे त्यक्तजीविताः।\nनानाशस्त्रप्रहरणाः सर्वे युद्धविशारदाः।।\n",
+            verse_meaning = "And there are many other heroes, equipped with various weapons and missiles, who have staked their lives for me, all skilled in warfare."
+        ),
+        Verse(
+            verse_id = 10,
+            verse_name = "Verse 10",
+            verse = "अपर्याप्तं तदस्माकं बलं भीष्माभिरक्षितम्।\nपर्याप्तं त्विदमेतेषां बलं भीमाभिरक्षितम्।।\n",
+            verse_meaning = "This army of ours, fully protected by Bhisma, is unconquerable; while that army of theirs, guarded in everyway by Bhima, is easy to conquer."
+        ),
+        Verse(
+            verse_id = 11,
+            verse_name = "Verse 11",
+            verse = "अयनेषु च सर्वेषु यथाभागमवस्थिताः।\nभीष्ममेवाभिरक्षन्तु भवन्तः सर्व एव हि।।\n",
+            verse_meaning = "Therefore, stationed in your respective positions on all fronts, do you all guard Bhisma in particular on all sides."
+        ),
+        Verse(
+            verse_id = 12,
+            verse_name = "Verse 12",
+            verse = "तस्य संजनयन्हर्षं कुरुवृद्धः पितामहः।\nसिंहनादं विनद्योच्चैः शङ्खं दध्मौ प्रतापवान्।।\n",
+            verse_meaning = "The grand old man of the Kaurava race, their glorious grand-patriarch Bhisma, cheering up Duryodhana, roared terribly like a lion and blew his conch."
+        ),
+        Verse(
+            verse_id = 13,
+            verse_name = "Verse 13",
+            verse = "ततः शङ्खाश्च भेर्यश्च पणवानकगोमुखाः।\nसहसैवाभ्यहन्यन्त स शब्दस्तुमुलोऽभवत्।।\n",
+            verse_meaning = "Then conchs, kettledrums, tabors, drums and trumpets suddenly blared forth and the noise was tumultuous."
+        ),
+        Verse(
+            verse_id = 14,
+            verse_name = "Verse 14",
+            verse = "ततः श्वेतैर्हयैर्युक्ते महति स्यन्दने स्थितौ।\nमाधवः पाण्डवश्चैव दिव्यौ शङ्खौ प्रदध्मतुः।।\n",
+            verse_meaning = "Then, seated in a glorious chariot drawn by white horses, Shri Krishna as well as Arjuna blew their celestial conchs."
+        ),
+        Verse(
+            verse_id = 15,
+            verse_name = "Verse 15",
+            verse = "पाञ्चजन्यं हृषीकेशो देवदत्तं धनंजयः।\nपौण्ड्रं दध्मौ महाशङ्खं भीमकर्मा वृकोदरः।।\n",
+            verse_meaning = "Shri Krishna blew His conch named Pancajanya; Arjuna, Devadatta; while Bh∂ma of ferocious deeds blew his mighty conch Paundra."
+        ),
+        Verse(
+            verse_id = 16,
+            verse_name = "Verse 16",
+            verse = "अनन्तविजयं राजा कुन्तीपुत्रो युधिष्ठिरः।\nनकुलः सहदेवश्च सुघोषमणिपुष्पकौ।।\n",
+            verse_meaning = "King Yudhishthira, son of Kunti, blew his conch Anantavijaya, while Nakula and Sahadeva blew theirs, known as Sughosha and Manipushpaka respectively."
+        ),
+        Verse(
+            verse_id = 17,
+            verse_name = "Verse 17",
+            verse = "काश्यश्च परमेष्वासः शिखण्डी च महारथः।\nधृष्टद्युम्नो विराटश्च सात्यकिश्चापराजितः।।\n",
+            verse_meaning = "And the excellent archer, the King of Kasi, and Sikhandi the Maharathi (the great chariot-warrior), Dhrstadyumna and Virata, and invincible Satyaki,"
+        ),
+        Verse(
+            verse_id = 18,
+            verse_name = "Verse 18",
+            verse = "द्रुपदो द्रौपदेयाश्च सर्वशः पृथिवीपते।\nसौभद्रश्च महाबाहुः शङ्खान्दध्मुः पृथक्पृथक्।।\n",
+            verse_meaning = "Drupada as well as the five sons of Draupadi, and the mighty-armed Abhimanyu, son of Subhadra, all of them, O lord of the earth, severally blew their respective conchs from all sides."
+        ),
+        Verse(
+            verse_id = 19,
+            verse_name = "Verse 19",
+            verse = "स घोषो धार्तराष्ट्राणां हृदयानि व्यदारयत्।\nनभश्च पृथिवीं चैव तुमुलो व्यनुनादयन्।।\n",
+            verse_meaning = "And the terrible sound, echoing through heaven and earth, rent the hearts of Dhrtarastra's army."
+        ),
+        Verse(
+            verse_id = 20,
+            verse_name = "Verse 20",
+            verse = "अथ व्यवस्थितान् दृष्ट्वा धार्तराष्ट्रान्कपिध्वजः।\nप्रवृत्ते शस्त्रसंपाते धनुरुद्यम्य पाण्डवः।।\n",
+            verse_meaning = "Now, O lord of the earth, seeing your sons arrayed against him and when missiles were ready to be hurled, Arjuna, who had the figure of Hanuman on the flag of his chariot, took up his bow"
+        ),
+        Verse(
+            verse_id = 21,
+            verse_name = "Verse 21",
+            verse = "हृषीकेशं तदा वाक्यमिदमाह महीपते।\n\nअर्जुन उवाच\n\nसेनयोरुभयोर्मध्ये रथं स्थापय मेऽच्युत।।\n",
+            verse_meaning = "and then addressed the following words to Shri Krishna : Krishna, place my chariot between the two armies."
+        ),
+        Verse(
+            verse_id = 22,
+            verse_name = "Verse 22",
+            verse = "यावदेतान्निरीक्षेऽहं योद्धुकामानवस्थितान्।\nकैर्मया सह योद्धव्यमस्मिन्रणसमुद्यमे।।\n",
+            verse_meaning = "And keep it there till I have carefully observed these warriors drawn up for battle, and have seen with whom I have to engage in this fight."
+        ),
+        Verse(
+            verse_id = 23,
+            verse_name = "Verse 23",
+            verse = "योत्स्यमानानवेक्षेऽहं य एतेऽत्र समागताः।\nधार्तराष्ट्रस्य दुर्बुद्धेर्युद्धे प्रियचिकीर्षवः।।\n",
+            verse_meaning = "I shall scan the well-wishers of evil-minded Duryodhana, in this war whoever have assembled on his side and are ready for the fight. "
+        ),
+        Verse(
+            verse_id = 24,
+            verse_name = "Verse 24",
+            verse = "संजय उवाच\n\nएवमुक्तो हृषीकेशो गुडाकेशेन भारत।\nसेनयोरुभयोर्मध्ये स्थापयित्वा रथोत्तमम्।।\n",
+            verse_meaning = "Sanjaya said: O king, thus addressed by Arjuna, Shri Krishna placed the magnificent chariot between the two armies"
+        ),
+        Verse(
+            verse_id = 25,
+            verse_name = "Verse 25",
+            verse = "भीष्मद्रोणप्रमुखतः सर्वेषां च महीक्षिताम्।\nउवाच पार्थ पश्यैतान्समवेतान्कुरूनिति।।\n",
+            verse_meaning = "in front of Bhisma, Drona and all the kings and said, Arjuna, behold these Kauravas assembled here."
+        ),
+        Verse(
+            verse_id = 26,
+            verse_name = "Verse 26",
+            verse = "तत्रापश्यत्स्थितान्पार्थः पितृ़नथ पितामहान्।\nआचार्यान्मातुलान्भ्रातृ़न्पुत्रान्पौत्रान्सखींस्तथा।।\n",
+            verse_meaning = "Now Arjuna saw stationed there in both the armies his uncles, grand-uncles and teachers, even great grand-uncles, maternal uncles, brothers and cousins, sons and nephews, and grand-nephews, even so friends, fathers-in-law and well-wishers as well. (26 & first half of 27)"
+        ),
+        Verse(
+            verse_id = 27,
+            verse_name = "Verse 27",
+            verse = "श्वशुरान्सुहृदश्चैव सेनयोरुभयोरपि।\nतान्समीक्ष्य स कौन्तेयः सर्वान्बन्धूनवस्थितान्।।\n",
+            verse_meaning = "Seeing all the relations present there, Arjuna was overcome with deep compassion and spoke thus in sorrow. (Second half of 27 and first half of 28)"
+        ),
+        Verse(
+            verse_id = 28,
+            verse_name = "Verse 28",
+            verse = "कृपया परयाऽऽविष्टो विषीदन्निदमब्रवीत्।\n\nअर्जुन उवाच\n\nदृष्ट्वेमं स्वजनं कृष्ण युयुत्सुं समुपस्थितम्।।\n",
+            verse_meaning = "Arjuna said: Krishna, as I see these kinsmen arrayed for battle,(SecoSecond half of 28)"
+        ),
+        Verse(
+            verse_id = 29,
+            verse_name = "Verse 29",
+            verse = "सीदन्ति मम गात्राणि मुखं च परिशुष्यति।\nवेपथुश्च शरीरे मे रोमहर्षश्च जायते।।\n",
+            verse_meaning = "my limbs give way, and my mouth is getting parched; nay, a shiver runs through my body and hair stands on end."
+        ),
+        Verse(
+            verse_id = 30,
+            verse_name = "Verse 30",
+            verse = "गाण्डीवं स्रंसते हस्तात्त्वक्चैव परिदह्यते।\nन च शक्नोम्यवस्थातुं भ्रमतीव च मे मनः।।\n",
+            verse_meaning = "The bow, Gandiva, slips from my hand and my skin too burns all over; my mind is whirling, as it were, and I can no longer hold myself steady."
+        ),
+        Verse(
+            verse_id = 31,
+            verse_name = "Verse 31",
+            verse = "निमित्तानि च पश्यामि विपरीतानि केशव।\nन च श्रेयोऽनुपश्यामि हत्वा स्वजनमाहवे।।\n",
+            verse_meaning = "And, Kesava, I see such omens of evil, nor do I see any good in killing my kinsmen in battle."
+        ),
+        Verse(
+            verse_id = 32,
+            verse_name = "Verse 32",
+            verse = "न काङ्क्षे विजयं कृष्ण न च राज्यं सुखानि च।\nकिं नो राज्येन गोविन्द किं भोगैर्जीवितेन वा।।\n",
+            verse_meaning = "Krishna, I do not covet victory, nor kingdom, nor pleasures. Govinda, of what use will kingdom or luxuries or even life be to us!"
+        ),
+        Verse(
+            verse_id = 33,
+            verse_name = "Verse 33",
+            verse = "येषामर्थे काङ्क्षितं नो राज्यं भोगाः सुखानि च।\nत इमेऽवस्थिता युद्धे प्राणांस्त्यक्त्वा धनानि च।।\n",
+            verse_meaning = "Those very persons for whose sake we covet\n" +
+                    "the kingdom, luxuries and pleasures"
+        ),
+        Verse(
+            verse_id = 34,
+            verse_name = "Verse 34",
+            verse = "आचार्याः पितरः पुत्रास्तथैव च पितामहाः।\nमातुलाः श्चशुराः पौत्राः श्यालाः सम्बन्धिनस्तथा।।\n",
+            verse_meaning = "teachers, uncles, sons and nephews and even so, grand- uncles and great grand-uncles, maternal uncles, fathers-in-law, grand-nephews, brothers-in-law and other relations - are here arrayed on the battlefield staking their lives and wealth."
+        ),
+        Verse(
+            verse_id = 35,
+            verse_name = "Verse 35",
+            verse = "एतान्न हन्तुमिच्छामि घ्नतोऽपि मधुसूदन।\nअपि त्रैलोक्यराज्यस्य हेतोः किं नु महीकृते।।\n",
+            verse_meaning = "O Slayer of Madhu, I do not want to kill them, even though they slay me, even for the sovereignty over the three worlds; how much the less for the kingdom here on earth!"
+        ),
+        Verse(
+            verse_id = 36,
+            verse_name = "Verse 36",
+            verse = "निहत्य धार्तराष्ट्रान्नः का प्रीतिः स्याज्जनार्दन।\nपापमेवाश्रयेदस्मान्हत्वैतानाततायिनः।।\n",
+            verse_meaning = "Krishna, how can we hope to be happy slaying the sons of Dhrtarastra; by killing even these desperadoes, sin will surely accrue to us."
+        ),
+        Verse(
+            verse_id = 37,
+            verse_name = "Verse 37",
+            verse = "तस्मान्नार्हा वयं हन्तुं धार्तराष्ट्रान्स्वबान्धवान्।\nस्वजनं हि कथं हत्वा सुखिनः स्याम माधव।।\n",
+            verse_meaning = "Therefore, Krishna, it does not behove us to kill our relations, the sons of Dhrtarastra. For, how can we be happy after killing our own kinsmen?"
+        ),
+        Verse(
+            verse_id = 38,
+            verse_name = "Verse 38",
+            verse = "यद्यप्येते न पश्यन्ति लोभोपहतचेतसः।\nकुलक्षयकृतं दोषं मित्रद्रोहे च पातकम्।।\n",
+            verse_meaning = "Even though these people, with their mind blinded by greed, perceive no evil in destroying their own race and no sin in treason to friends,"
+        ),
+        Verse(
+            verse_id = 39,
+            verse_name = "Verse 39",
+            verse = "कथं न ज्ञेयमस्माभिः पापादस्मान्निवर्तितुम्।\nकुलक्षयकृतं दोषं प्रपश्यद्भिर्जनार्दन।।\n",
+            verse_meaning = "why should not we, O Krishna, who see clearly the sin accruing from the destruction of oneís family, think of desisting from committing this foul deed."
+        ),
+        Verse(
+            verse_id = 40,
+            verse_name = "Verse 40",
+            verse = "कुलक्षये प्रणश्यन्ति कुलधर्माः सनातनाः।\nधर्मे नष्टे कुलं कृत्स्नमधर्मोऽभिभवत्युत।।\n",
+            verse_meaning = "Age-long family traditions disappear with the destruction of a family; and virtue having been lost, vice takes hold of the entire race."
+        ),
+        Verse(
+            verse_id = 41,
+            verse_name = "Verse 41",
+            verse = "अधर्माभिभवात्कृष्ण प्रदुष्यन्ति कुलस्त्रियः।\nस्त्रीषु दुष्टासु वार्ष्णेय जायते वर्णसङ्करः।।\n",
+            verse_meaning = "With the preponderance of vice, Krishna, the women of the family become corrupt; and with the corruption of women, O descendant of Vrsni, there ensues an intermixture of castes."
+        ),
+        Verse(
+            verse_id = 42,
+            verse_name = "Verse 42",
+            verse = "सङ्करो नरकायैव कुलघ्नानां कुलस्य च।\nपतन्ति पितरो ह्येषां लुप्तपिण्डोदकक्रियाः।।\n",
+            verse_meaning = "Progeny due to promiscuity damns the destroyers of the race as well as the race itself. Deprived of the offerings of rice and water (Sraddha, Tarpana etc.,) the manes of their race also fall."
+        ),
+        Verse(
+            verse_id = 43,
+            verse_name = "Verse 43",
+            verse = "दोषैरेतैः कुलघ्नानां वर्णसङ्करकारकैः।\nउत्साद्यन्ते जातिधर्माः कुलधर्माश्च शाश्वताः।।\n",
+            verse_meaning = "Through these evils bringing about an intermixture of castes, the age-long caste traditions and family customs of the killers of kinsmen get extinct."
+        ),
+        Verse(
+            verse_id = 44,
+            verse_name = "Verse 44",
+            verse = "उत्सन्नकुलधर्माणां मनुष्याणां जनार्दन।\nनरकेऽनियतं वासो भवतीत्यनुशुश्रुम।।\n",
+            verse_meaning = "Krishna, we hear that men who have lost their family traditions dwell in hell for an indefinite period of time."
+        ),
+        Verse(
+            verse_id = 45,
+            verse_name = "Verse 45",
+            verse = "अहो बत महत्पापं कर्तुं व्यवसिता वयम्।\nयद्राज्यसुखलोभेन हन्तुं स्वजनमुद्यताः।।\n",
+            verse_meaning = "Oh what a pity! Though possessed of intelligence we have set our mind on the commission of a great sin; that due to lust for throne and enjoyment we are intent on killing our own kinsmen. (45)"
+        ),
+        Verse(
+            verse_id = 46,
+            verse_name = "Verse 46",
+            verse = "यदि मामप्रतीकारमशस्त्रं शस्त्रपाणयः।\nधार्तराष्ट्रा रणे हन्युस्तन्मे क्षेमतरं भवेत्।।\n",
+            verse_meaning = "It would be better for me if the sons of Dhrtarastra, armed with weapons, kill me in battle, while I am unarmed and unresisting."
+        ),
+        Verse(
+            verse_id = 47,
+            verse_name = "Verse 47",
+            verse = "सञ्जय उवाच\n\nएवमुक्त्वाऽर्जुनः संख्ये रथोपस्थ उपाविशत्।\nविसृज्य सशरं चापं शोकसंविग्नमानसः।।\n",
+            verse_meaning = "Sanjaya said: Arjuna, whose mind was agitated by grief on the battlefield, having spoken thus, and having cast aside his bow and arrows, sank into the hinder part of his chariot."
         )
     )
 }
