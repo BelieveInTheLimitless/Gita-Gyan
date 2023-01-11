5441ce33f520de1e4953c2569d493053e37dde4c
Vishal Patil <believeinthelimitless@gmail.com>
2023-01-11 18:38:38 +0530

    Added English Chapter 4

--- app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter4.kt
+++ app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter4.kt
@@ -7,8 +7,254 @@ fun getEnglishChapter4(): List<Verse> {
         Verse(
             verse_id = 1,
             verse_name = "Verse 1",
-            verse = "श्री भगवानुवाच\n\nइमं विवस्वते योगं प्रोक्तवानहमव्ययम्।\nविवस्वान् मनवे प्राह मनुरिक्ष्वाकवेऽब्रवीत्।।4.1।।\n",
-            verse_meaning = "Shri Bhagavan said: I revealed this immortal Yoga to Vivasvan (Sun-god); Vivasvan conveyed it to Manu (his son); and Manu imparted it to (his son) Ikshwaku."
+            verse = "श्री भगवानुवाच\n\nइमं विवस्वते योगं प्रोक्तवानहमव्ययम्।\nविवस्वान् मनवे प्राह मनुरिक्ष्वाकवेऽब्रवीत्।।\n",
+            verse_meaning = "Shri Bhagavan said: I revealed this immortal Yoga to Vivasvan (Sun-god); Vivasvan conveyed it to Manu (his son); and Manu imparted it to\n (his son) Iksvaku."
+        ),
+        Verse(
+            verse_id = 2,
+            verse_name = "Verse 2",
+            verse = "एवं परम्पराप्राप्तमिमं राजर्षयो विदुः।\nस कालेनेह महता योगो नष्टः परन्तप।।\n",
+            verse_meaning = "Thus transmitted in succession from father to son, Arjuna, this Yoga remained known to the Rajarsis (royal sages). Through long lapse of time, this Yoga got lost to the world"
+        ),
+        Verse(
+            verse_id = 3,
+            verse_name = "Verse 3",
+            verse = "स एवायं मया तेऽद्य योगः प्रोक्तः पुरातनः।\nभक्तोऽसि मे सखा चेति रहस्यं ह्येतदुत्तमम्।।\n",
+            verse_meaning = "The same ancient Yoga, which is the supreme secret, has this day been imparted to you by Me, because you are My devotee and friend."
+        ),
+        Verse(
+            verse_id = 4,
+            verse_name = "Verse 4",
+            verse = "अर्जुन उवाच\n\nअपरं भवतो जन्म परं जन्म विवस्वतः।\nकथमेतद्विजानीयां त्वमादौ प्रोक्तवानिति।।\n",
+            verse_meaning = "Arjuna said: You are of recent origin, while the birth of Vivasvan dates back to remote antiquity. How, then, am I to believe that You imparted this Yoga at the beginning of the creation!"
+        ),
+        Verse(
+            verse_id = 5,
+            verse_name = "Verse 5",
+            verse = "श्री भगवानुवाच\n\nबहूनि मे व्यतीतानि जन्मानि तव चार्जुन।\nतान्यहं वेद सर्वाणि न त्वं वेत्थ परन्तप।।\n",
+            verse_meaning = "Shri Bhagavan said : Arjuna, you and I have passed through many births, I remember them all; you do not remember, O chastiser of foes."
+        ),
+        Verse(
+            verse_id = 6,
+            verse_name = "Verse 6",
+            verse = "अजोऽपि सन्नव्ययात्मा भूतानामीश्वरोऽपि सन्।\nप्रकृतिं स्वामधिष्ठाय संभवाम्यात्ममायया।।\n",
+            verse_meaning = "Though birthless and immortal and the Lord of all beings, I manifest Myself through My own Yogamaya (divine potency), keeping My nature (Prakrti) under control."
+        ),
+        Verse(
+            verse_id = 7,
+            verse_name = "Verse 7",
+            verse = "यदा यदा हि धर्मस्य ग्लानिर्भवति भारत।\nअभ्युत्थानमधर्मस्य तदाऽऽत्मानं सृजाम्यहम्।।\n",
+            verse_meaning = "Arjuna, whenever righteousness is on the decline, unrighteousness is in the ascendant, then I body Myself forth."
+        ),
+        Verse(
+            verse_id = 8,
+            verse_name = "Verse 8",
+            verse = "परित्राणाय साधूनां विनाशाय च दुष्कृताम्।\nधर्मसंस्थापनार्थाय संभवामि युगे युगे।।\n",
+            verse_meaning = "For the protection of the virtuous, for the extirpation of evil-doers, and for establishing Dharma (righteousness) on a firm footing, I manifest Myself from age to age."
+        ),
+        Verse(
+            verse_id = 9,
+            verse_name = "Verse 9",
+            verse = "जन्म कर्म च मे दिव्यमेवं यो वेत्ति तत्त्वतः।\nत्यक्त्वा देहं पुनर्जन्म नैति मामेति सोऽर्जुन।।\n",
+            verse_meaning = "Arjuna, My birth and activities are divine. He who knows this in reality is not reborn on leaving his body, but comes to Me."
+        ),
+        Verse(
+            verse_id = 10,
+            verse_name = "Verse 10",
+            verse = "वीतरागभयक्रोधा मन्मया मामुपाश्रिताः।\nबहवो ज्ञानतपसा पूता मद्भावमागताः।।\n",
+            verse_meaning = "Completely rid of attachment, fear and anger, wholly absorbed in Me, depending on Me, and purified by the penance of wisdom, many have become one with Me even in the past."
+        ),
+        Verse(
+            verse_id = 11,
+            verse_name = "Verse 11",
+            verse = "ये यथा मां प्रपद्यन्ते तांस्तथैव भजाम्यहम्।\nमम वर्त्मानुवर्तन्ते मनुष्याः पार्थ सर्वशः।।\n",
+            verse_meaning = "Arjuna, howsoever men seek Me, even so do I respond to them; for all men follow My path in everyway."
+        ),
+        Verse(
+            verse_id = 12,
+            verse_name = "Verse 12",
+            verse = "काङ्क्षन्तः कर्मणां सिद्धिं यजन्त इह देवताः।\nक्षिप्रं हि मानुषे लोके सिद्धिर्भवति कर्मजा।।\n",
+            verse_meaning = "In this world of human beings, men seeking the fruition of their activities, worship the gods; for success born of actions follows quickly."
+        ),
+        Verse(
+            verse_id = 13,
+            verse_name = "Verse 13",
+            verse = "चातुर्वर्ण्यं मया सृष्टं गुणकर्मविभागशः।\nतस्य कर्तारमपि मां विद्ध्यकर्तारमव्ययम्।।\n",
+            verse_meaning = "The four orders of society (viz., the Brahmana, the Ksatriya, the Vaisya and the Sudra) were created by Me, classifying them according to the Gunas predominant in each and apportioning corresponding duties to them; though the originator of this creation, know Me, the Immortal Lord, to be a non-doer."
+        ),
+        Verse(
+            verse_id = 14,
+            verse_name = "Verse 14",
+            verse = "न मां कर्माणि लिम्पन्ति न मे कर्मफले स्पृहा।\nइति मां योऽभिजानाति कर्मभिर्न स बध्यते।।\n",
+            verse_meaning = "Since I have no craving for the fruit of actions, actions do not taint Me. Even he who thus knows Me in reality is not bound by actions."
+        ),
+        Verse(
+            verse_id = 15,
+            verse_name = "Verse 15",
+            verse = "एवं ज्ञात्वा कृतं कर्म पूर्वैरपि मुमुक्षुभिः।\nकुरु कर्मैव तस्मात्त्वं पूर्वैः पूर्वतरं कृतम्।।\n",
+            verse_meaning = "Having known thus, action was performed even by the ancient seekers for liberation; therefore, do you also perform actions as have been performed by the ancients from antiquity."
+        ),
+        Verse(
+            verse_id = 16,
+            verse_name = "Verse 16",
+            verse = "किं कर्म किमकर्मेति कवयोऽप्यत्र मोहिताः।\nतत्ते कर्म प्रवक्ष्यामि यज्ज्ञात्वा मोक्ष्यसेऽशुभात्।।\n",
+            verse_meaning = "What is action and what is inaction? Even men of intelligence are puzzled over this question. Therefore, I shall expound to you the truth about action, knowing which you will be freed from its evil effects i.e., the shackles of karma."
+        ),
+        Verse(
+            verse_id = 17,
+            verse_name = "Verse 17",
+            verse = "कर्मणो ह्यपि बोद्धव्यं बोद्धव्यं च विकर्मणः।\nअकर्मणश्च बोद्धव्यं गहना कर्मणो गतिः।।\n",
+            verse_meaning = "The truth about action must be known and the truth of inaction also must be known; even so, the truth about prohibited action (Vikarma) must be known. For, mysterious are the ways of action."
+        ),
+        Verse(
+            verse_id = 18,
+            verse_name = "Verse 18",
+            verse = "कर्मण्यकर्म यः पश्येदकर्मणि च कर्म यः।\nस बुद्धिमान् मनुष्येषु स युक्तः कृत्स्नकर्मकृत्।।\n",
+            verse_meaning = "He who sees inaction in action, and action in inaction, is wise among men; he is a Yogi, who has performed all actions."
+        ),
+        Verse(
+            verse_id = 19,
+            verse_name = "Verse 19",
+            verse = "यस्य सर्वे समारम्भाः कामसङ्कल्पवर्जिताः।\nज्ञानाग्निदग्धकर्माणं तमाहुः पण्डितं बुधाः।।\n",
+            verse_meaning = "Even the wise call him a sage, whose undertakings are all free from desire and Sankalpa (thoughts of the world) and whose actions are burnt up by the fire of wisdom."
+        ),
+        Verse(
+            verse_id = 20,
+            verse_name = "Verse 20",
+            verse = "त्यक्त्वा कर्मफलासङ्गं नित्यतृप्तो निराश्रयः।\nकर्मण्यभिप्रवृत्तोऽपि नैव किञ्चित्करोति सः।।\n",
+            verse_meaning = "He, who, having totally given up attachment to actions and their fruit, no longer depends on anything in the world, and is ever content, does nothing at all, though fully engaged in action."
+        ),
+        Verse(
+            verse_id = 21,
+            verse_name = "Verse 21",
+            verse = "निराशीर्यतचित्तात्मा त्यक्तसर्वपरिग्रहः।\nशारीरं केवलं कर्म कुर्वन्नाप्नोति किल्बिषम्।।\n",
+            verse_meaning = "Having subdued his mind and body, and giving up all objects of enjoyment, and free from craving, he who performs sheer bodily action, does not incur sin."
+        ),
+        Verse(
+            verse_id = 22,
+            verse_name = "Verse 22",
+            verse = "यदृच्छालाभसन्तुष्टो द्वन्द्वातीतो विमत्सरः।\nसमः सिद्धावसिद्धौ च कृत्वापि न निबध्यते।।\n",
+            verse_meaning = "The Karmayogi, who is contented with whatever is got unsought, is free from jealousy and has transcended all pairs of opposites like joy and grief, and is balanced in success and failure, is not bound by his action."
+        ),
+        Verse(
+            verse_id = 23,
+            verse_name = "Verse 23",
+            verse = "गतसङ्गस्य मुक्तस्य ज्ञानावस्थितचेतसः।\nयज्ञायाचरतः कर्म समग्रं प्रविलीयते।।\n",
+            verse_meaning = "All his actions get dissolved entirely, who is free from attachment and has no identification with the body; and free from the feeling of mine, whose mind is established in the knowledge of Self and who works merely for the sake of sacrifice."
+        ),
+        Verse(
+            verse_id = 24,
+            verse_name = "Verse 24",
+            verse = "ब्रह्मार्पणं ब्रह्महविर्ब्रह्माग्नौ ब्रह्मणा हुतम्।\nब्रह्मैव तेन गन्तव्यं ब्रह्मकर्मसमाधिना।।\n",
+            verse_meaning = "In the practice of seeing Brahma everywhere as a form of sacrifice, Brahma is the ladle (with which oblation is poured into the fire, etc.); Brahma, again, is the oblation; Brahma is the fire, Brahma itself is the sacrificer and so Brahma itself constitutes the act of pouring the oblation into the fire. And finally Brahma is the goal to be reached by him who is absorbed in Brahma as the act of such sacrifice."
+        ),
+        Verse(
+            verse_id = 25,
+            verse_name = "Verse 25",
+            verse = "दैवमेवापरे यज्ञं योगिनः पर्युपासते।\nब्रह्माग्नावपरे यज्ञं यज्ञेनैवोपजुह्वति।।\n",
+            verse_meaning = "Other Yogis duly offer sacrifice only in the shape of worship to gods, while others perform sacrifice by offering the self by the Self itself in the fire of Brahma through the sacrifice known as the perception of identity."
+        ),
+        Verse(
+            verse_id = 26,
+            verse_name = "Verse 26",
+            verse = "श्रोत्रादीनीन्द्रियाण्यन्ये संयमाग्निषु जुह्वति।\nशब्दादीन्विषयानन्य इन्द्रियाग्निषु जुह्वति।।\n",
+            verse_meaning = "Others offer as sacrifice their senses of hearing etc., into the fires of self-discipline. Other Yog∂s, again, offer sound and other objects of perception into the fires of the senses."
+        ),
+        Verse(
+            verse_id = 27,
+            verse_name = "Verse 27",
+            verse = "सर्वाणीन्द्रियकर्माणि प्राणकर्माणि चापरे।\nआत्मसंयमयोगाग्नौ जुह्वति ज्ञानदीपिते।।\n",
+            verse_meaning = "Others sacrifice all the functions of their senses and the functions of the vital airs (Prana) into the fire of Yoga in the shape of self-control, kindled by wisdom."
+        ),
+        Verse(
+            verse_id = 28,
+            verse_name = "Verse 28",
+            verse = "द्रव्ययज्ञास्तपोयज्ञा योगयज्ञास्तथापरे।\nस्वाध्यायज्ञानयज्ञाश्च यतयः संशितव्रताः।।\n",
+            verse_meaning = "Some perform sacrifice with material possessions; some offer sacrifice in the shape of austerities; others sacrifice through the practice of Yoga; while some striving souls, observing austere vows, perform sacrifice in the shape of wisdom through the study of sacred texts."
+        ),
+        Verse(
+            verse_id = 29,
+            verse_name = "Verse 29",
+            verse = "अपाने जुह्वति प्राण प्राणेऽपानं तथाऽपरे।\nप्राणापानगती रुद्ध्वा प्राणायामपरायणाः।।\n",
+            verse_meaning = "Continued on verse 30"
+        ),
+        Verse(
+            verse_id = 30,
+            verse_name = "Verse 30",
+            verse = "अपरे नियताहाराः प्राणान्प्राणेषु जुह्वति।\nसर्वेऽप्येते यज्ञविदो यज्ञक्षपितकल्मषाः।।\n",
+            verse_meaning = "Other Yog∂s offer the act of exhalation into that of inhalation; even so, others the act of inhalation into that of exhalation. There are still others given to the practice of Pranayama (breath-control), who having regulated their diet and controlled the processes of exhalation and inhalation both pour their vital airs into the vital airs themselves. All these have their sins consumed away by sacrifice and understand the meaning of sacrificial worship. (29 and 30)"
+        ),
+        Verse(
+            verse_id = 31,
+            verse_name = "Verse 31",
+            verse = "यज्ञशिष्टामृतभुजो यान्ति ब्रह्म सनातनम्।\nनायं लोकोऽस्त्ययज्ञस्य कुतो़ऽन्यः कुरुसत्तम।।\n",
+            verse_meaning = "Arjuna, Yogis who enjoy the nectar that has been left over after the performance of a sacrifice attain the eternal Brahma. To the man who does not offer sacrifice, even this world is not happy; how, then, can the other world be happy?"
+        ),
+        Verse(
+            verse_id = 32,
+            verse_name = "Verse 32",
+            verse = "एवं बहुविधा यज्ञा वितता ब्रह्मणो मुखे।\nकर्मजान्विद्धि तान्सर्वानेवं ज्ञात्वा विमोक्ष्यसे।।\n",
+            verse_meaning = "Many such forms of sacrifice have been set forth in detail in the Vedas; know them all as involving the action of mind, senses and body. Thus, knowing the truth about them you shall be freed from the bondage of action (through their performance)."
+        ),
+        Verse(
+            verse_id = 33,
+            verse_name = "Verse 33",
+            verse = "श्रेयान्द्रव्यमयाद्यज्ञाज्ज्ञानयज्ञः परन्तप।\nसर्वं कर्माखिलं पार्थ ज्ञाने परिसमाप्यते।।\n",
+            verse_meaning = "Arjuna, sacrifice through Knowledge is superior to sacrifice performed with material things. For all actions without exception culminate in Knowledge, O son of Kunti."
+        ),
+        Verse(
+            verse_id = 34,
+            verse_name = "Verse 34",
+            verse = "तद्विद्धि प्रणिपातेन परिप्रश्नेन सेवया।\nउपदेक्ष्यन्ति ते ज्ञानं ज्ञानिनस्तत्त्वदर्शिनः।।\n",
+            verse_meaning = "Understand the true nature of that Knowledge by approaching illumined soul. If you prostrate at their feet, render them service, and question them with an open and guileless heart, those wise seers of Truth will instruct you in that Knowledge."
+        ),
+        Verse(
+            verse_id = 35,
+            verse_name = "Verse 35",
+            verse = "यज्ज्ञात्वा न पुनर्मोहमेवं यास्यसि पाण्डव।\nयेन भूतान्यशेषेण द्रक्ष्यस्यात्मन्यथो मयि।।\n",
+            verse_meaning = "Arjuna, when you have achieved enlightenment, ignorance will delude you no more. In the light of that knowledge, you will see the entire creation first within your own Self, and then in Me (the Oversoul)."
+        ),
+        Verse(
+            verse_id = 36,
+            verse_name = "Verse 36",
+            verse = "अपि चेदसि पापेभ्यः सर्वेभ्यः पापकृत्तमः।\nसर्वं ज्ञानप्लवेनैव वृजिनं सन्तरिष्यसि।।\n",
+            verse_meaning = "Even if you were the most sinful of all sinners, this Knowledge alone would carry you, like a raft, across all your sins."
+        ),
+        Verse(
+            verse_id = 37,
+            verse_name = "Verse 37",
+            verse = "यथैधांसि समिद्धोऽग्निर्भस्मसात्कुरुतेऽर्जुन।\nज्ञानाग्निः सर्वकर्माणि भस्मसात्कुरुते तथा।।\n",
+            verse_meaning = "For, as the blazing fire turns the fuel to ashes, Arjuna, even so the fire of Knowledge turns all actions to ashes."
+        ),
+        Verse(
+            verse_id = 38,
+            verse_name = "Verse 38",
+            verse = "न हि ज्ञानेन सदृशं पवित्रमिह विद्यते।\nतत्स्वयं योगसंसिद्धः कालेनात्मनि विन्दति।।\n",
+            verse_meaning = "In this world there is no purifier as great as Knowledge; he who has attained purity of heart through prolonged practice of Karmayoga, automatically sees the light of Truth in the self in course of time."
+        ),
+        Verse(
+            verse_id = 39,
+            verse_name = "Verse 39",
+            verse = "श्रद्धावाँल्लभते ज्ञानं तत्परः संयतेन्द्रियः।\nज्ञानं लब्ध्वा परां शान्तिमचिरेणाधिगच्छति।।\n",
+            verse_meaning = "He who has mastered his senses, is exclusively devoted to his practice and is full of faith, attains Knowledge; having had the revelation of Truth, he immediately attains supreme peace in the form of God-realization."
+        ),
+        Verse(
+            verse_id = 40,
+            verse_name = "Verse 40",
+            verse = "अज्ञश्चाश्रद्दधानश्च संशयात्मा विनश्यति।\nनायं लोकोऽस्ति न परो न सुखं संशयात्मनः।।\n",
+            verse_meaning = "He who lacks discrimination, is devoid of faith, and is at the same time possessed by doubt, is lost to the spiritual path. For the doubting soul there is neither this world nor the world beyond, nor even happiness."
+        ),
+        Verse(
+            verse_id = 41,
+            verse_name = "Verse 41",
+            verse = "योगसंन्यस्तकर्माणं ज्ञानसंछिन्नसंशयम्।\nआत्मवन्तं न कर्माणि निबध्नन्ति धनञ्जय।।\n",
+            verse_meaning = "Arjuna, actions do not bind him who has dedicated all his actions to God according to the spirit of Karmayoga, whose doubts have been dispelled by wisdom and who is self-possessed."
+        ),
+        Verse(
+            verse_id = 42,
+            verse_name = "Verse 42",
+            verse = "तस्मादज्ञानसंभूतं हृत्स्थं ज्ञानासिनाऽऽत्मनः।\nछित्त्वैनं संशयं योगमातिष्ठोत्तिष्ठ भारत।।\n",
+            verse_meaning = "Therefore, Arjuna slashing to pieces, with the sword of knowledge, this doubt in your heart, born of ignorance, establish yourself in Karmayoga in the shape of even-mindedness, and stand up for the fight."
         )
     )
 }
