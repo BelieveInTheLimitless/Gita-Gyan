3b929aac3547441c1982790300d989360edf8033
Vishal Patil <believeinthelimitless@gmail.com>
2023-01-13 22:26:05 +0530

    Added English Chapter 5

--- app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter5.kt
+++ app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter5.kt
@@ -7,8 +7,176 @@ fun getEnglishChapter5(): List<Verse>{
         Verse(
             verse_id = 1,
             verse_name = "Verse 1",
-            verse = "अर्जुन उवाच\n\nसंन्यासं कर्मणां कृष्ण पुनर्योगं च शंससि।\nयच्छ्रेय एतयोरेकं तन्मे ब्रूहि सुनिश्िचतम्।।5.1।।\n",
+            verse = "अर्जुन उवाच\n\nसंन्यासं कर्मणां कृष्ण पुनर्योगं च शंससि।\nयच्छ्रेय एतयोरेकं तन्मे ब्रूहि सुनिश्िचतम्।।\n",
             verse_meaning = "Arjuna said : Krishna, you extol Sankhyayoga (the Yoga of Knowledge) and then the Yoga of Action. Pray, tell me which of the two is decidedly conducive to my good."
+        ),
+        Verse(
+            verse_id = 2,
+            verse_name = "Verse 2",
+            verse = "श्री भगवानुवाच\n\nसंन्यासः कर्मयोगश्च निःश्रेयसकरावुभौ।\nतयोस्तु कर्मसंन्यासात्कर्मयोगो विशिष्यते।।\n",
+            verse_meaning = "Shri Bhagavan said : The Yoga of Knowledge and the Yoga of Action both lead to supreme Bliss. Of the two, however, the Yoga of Action, being easier of practice, is superior to the Yoga of Knowledge."
+        ),
+        Verse(
+            verse_id = 3,
+            verse_name = "Verse 3",
+            verse = "ज्ञेयः स नित्यसंन्यासी यो न द्वेष्टि न काङ्क्षति।\nनिर्द्वन्द्वो हि महाबाहो सुखं बन्धात्प्रमुच्यते।।\n",
+            verse_meaning = "The Karmayogi who neither hates nor desires should ever be considered a man of renunciation. For, Arjuna, he who is free from the pairs of opposites is easily liberated from bondage."
+        ),
+        Verse(
+            verse_id = 4,
+            verse_name = "Verse 4",
+            verse = "सांख्ययोगौ पृथग्बालाः प्रवदन्ति न पण्डिताः।\nएकमप्यास्थितः सम्यगुभयोर्विन्दते फलम्।।\n",
+            verse_meaning = "It is the ignorant, not the wise, who say that Sankhyayoga and Karmayoga lead to divergent results. For, one who is firmly established in either, gets the fruit of both which is the same, viz., God-realization."
+        ),
+        Verse(
+            verse_id = 5,
+            verse_name = "Verse 5",
+            verse = "यत्सांख्यैः प्राप्यते स्थानं तद्योगैरपि गम्यते।\nएकं सांख्यं च योगं च यः पश्यति स पश्यति।।\n",
+            verse_meaning = "The (supreme) state which is reached by the Sankhyayog∂ is attained also by the Karmayog∂. Therefore, he alone who sees Sankhyayoga and Karmayoga as identical so far as their result goes, sees truly."
+        ),
+        Verse(
+            verse_id = 6,
+            verse_name = "Verse 6",
+            verse = "संन्यासस्तु महाबाहो दुःखमाप्तुमयोगतः।\nयोगयुक्तो मुनिर्ब्रह्म नचिरेणाधिगच्छति।।\n",
+            verse_meaning = "Without Karmayoga, however, Sankhyayoga i.e., renunciation of doership in relation to all activities of the mind, senses and body is difficult to accomplish; whereas the Karmayogi, who keeps his mind fixed on God, reaches Brahma in no time, Arjuna."
+        ),
+        Verse(
+            verse_id = 7,
+            verse_name = "Verse 7",
+            verse = "योगयुक्तो विशुद्धात्मा विजितात्मा जितेन्द्रियः।\nसर्वभूतात्मभूतात्मा कुर्वन्नपि न लिप्यते।।\n",
+            verse_meaning = "The Karmayogi, who has fully conquered his mind and mastered his senses, whose heart is pure, and who has identified himself with the Self of all beings (viz., God), remains untainted, even though performing action."
+        ),
+        Verse(
+            verse_id = 8,
+            verse_name = "Verse 8",
+            verse = "नैव किंचित्करोमीति युक्तो मन्येत तत्त्ववित्।\nपश्यन् श्रृणवन्स्पृशञ्जिघ्रन्नश्नन्गच्छन्स्वपन् श्वसन्।।\n",
+            verse_meaning = "Continued on Verse 9"
+        ),
+        Verse(
+            verse_id = 9,
+            verse_name = "Verse 9",
+            verse = "प्रलपन्विसृजन्गृह्णन्नुन्मिषन्निमिषन्नपि।\nइन्द्रियाणीन्द्रियार्थेषु वर्तन्त इति धारयन्।।\n",
+            verse_meaning = "However, the Sankhyayogi, who knows the reality of things, must believe that he does nothing, even though seeing, hearing, touching, smelling, eating or drinking, walking, sleeping, breathing, speaking, answering the calls of nature, grasping, and opening or closing the eyes, holding that it is the senses alone that are moving among their objects. (8 and 9)"
+        ),
+        Verse(
+            verse_id = 10,
+            verse_name = "Verse 10",
+            verse = "ब्रह्मण्याधाय कर्माणि सङ्गं त्यक्त्वा करोति यः।\nलिप्यते न स पापेन पद्मपत्रमिवाम्भसा।।\n",
+            verse_meaning = "He who acts offering all actions to God, and shaking off attachment, remains untouched by sin, as the lotus leaf by water."
+        ),
+        Verse(
+            verse_id = 11,
+            verse_name = "Verse 11",
+            verse = "कायेन मनसा बुद्ध्या केवलैरिन्द्रियैरपि।\nयोगिनः कर्म कुर्वन्ति सङ्गं त्यक्त्वाऽऽत्मशुद्धये।।\n",
+            verse_meaning = "The Karmayogis perform action only with their senses, mind, intellect and body as well, without the feeling of mine in respect of them and shaking off attachment, simply for the sake of self-purification."
+        ),
+        Verse(
+            verse_id = 12,
+            verse_name = "Verse 12",
+            verse = "युक्तः कर्मफलं त्यक्त्वा शान्तिमाप्नोति नैष्ठिकीम्।\nअयुक्तः कामकारेण फले सक्तो निबध्यते।।\n",
+            verse_meaning = "Offering the fruit of actions to God, the Karmayogi attains everlasting peace in the shape of God-realization; whereas, he who works with a selfish motive, being attached to the fruit of actions through desire, gets tied down."
+        ),
+        Verse(
+            verse_id = 13,
+            verse_name = "Verse 13",
+            verse = "सर्वकर्माणि मनसा संन्यस्यास्ते सुखं वशी।\nनवद्वारे पुरे देही नैव कुर्वन्न कारयन्।।\n",
+            verse_meaning = "The self-controlled Sankhyayogi, doing nothing himself and getting nothing done by others, rests happily in God - the embodiment of Truth, Knowledge and Bliss, mentally relegating all actions to the mansion of nine gates (the body with nine openings)."
+        ),
+        Verse(
+            verse_id = 14,
+            verse_name = "Verse 14",
+            verse = "न कर्तृत्वं न कर्माणि लोकस्य सृजति प्रभुः।\nन कर्मफलसंयोगं स्वभावस्तु प्रवर्तते।।\n",
+            verse_meaning = "God determines neither the doership nor the doings of men, nor even their contact with the fruit of actions; but it is Nature alone that functions."
+        ),
+        Verse(
+            verse_id = 15,
+            verse_name = "Verse 15",
+            verse = "नादत्ते कस्यचित्पापं न चैव सुकृतं विभुः।\nअज्ञानेनावृतं ज्ञानं तेन मुह्यन्ति जन्तवः।।\n",
+            verse_meaning = "The omnipresent God does not partake the virtue or sin of anyone. Knowledge is enveloped by ignorance; hence it is that beings are constantly falling a prey to delusion."
+        ),
+        Verse(
+            verse_id = 16,
+            verse_name = "Verse 16",
+            verse = "ज्ञानेन तु तदज्ञानं येषां नाशितमात्मनः।\nतेषामादित्यवज्ज्ञानं प्रकाशयति तत्परम्।।\n",
+            verse_meaning = "In the case, however, of those whose said ignorance has been destroyed by true knowledge of God, that wisdom shining like the sun reveals the Supreme."
+        ),
+        Verse(
+            verse_id = 17,
+            verse_name = "Verse 17",
+            verse = "तद्बुद्धयस्तदात्मानस्तन्निष्ठास्तत्परायणाः।\nगच्छन्त्यपुनरावृत्तिं ज्ञाननिर्धूतकल्मषाः।।\n",
+            verse_meaning = "Those whose mind and intellect are wholly merged in Him, who remain constantly established in identity with Him, and have finally become one with Him, their sins being wiped out by wisdom, reach the supreme goal whence there is no return."
+        ),
+        Verse(
+            verse_id = 18,
+            verse_name = "Verse 18",
+            verse = "विद्याविनयसंपन्ने ब्राह्मणे गवि हस्तिनि।\nशुनि चैव श्वपाके च पण्डिताः समदर्शिनः।।\n",
+            verse_meaning = "The wise look with equanimity on all whether it be a Brahmana endowed with learning and culture, a cow, an elephant, a dog and a pariah, too."
+        ),
+        Verse(
+            verse_id = 19,
+            verse_name = "Verse 19",
+            verse = "इहैव तैर्जितः सर्गो येषां साम्ये स्थितं मनः।\nनिर्दोषं हि समं ब्रह्म तस्माद्ब्रह्मणि ते स्थिताः।।\n",
+            verse_meaning = "Even here is the mortal plane conquered by those whose mind is established in unity; since the Absolute is untouched by evil and is the same to all, hence they are established in the Eternal."
+        ),
+        Verse(
+            verse_id = 20,
+            verse_name = "Verse 20",
+            verse = "न प्रहृष्येत्प्रियं प्राप्य नोद्विजेत्प्राप्य चाप्रियम्।\nस्थिरबुद्धिरसम्मूढो ब्रह्मविद्ब्रह्मणि स्थितः।।\n",
+            verse_meaning = "He who, with firm intellect and free from doubt, rejoices not on obtaining what is pleasant and does not feel perturbed on meeting with the unpleasant, that knower of Brahma lives eternally in identity with Brahma."
+        ),
+        Verse(
+            verse_id = 21,
+            verse_name = "Verse 21",
+            verse = "बाह्यस्पर्शेष्वसक्तात्मा विन्दत्यात्मनि यत्सुखम्।\nस ब्रह्मयोगयुक्तात्मा सुखमक्षयमश्नुते।।\n",
+            verse_meaning = "He whose mind remains unattached to sense-objects, derives through meditation the Sattvika joy which dwells in the mind; then that Yogi, having completely identified himself through meditation with Brahma, enjoys eternal Bliss."
+        ),
+        Verse(
+            verse_id = 22,
+            verse_name = "Verse 22",
+            verse = "ये हि संस्पर्शजा भोगा दुःखयोनय एव ते।\nआद्यन्तवन्तः कौन्तेय न तेषु रमते बुधः।।\n",
+            verse_meaning = "The pleasures which are born of sense-contacts are verily a source of suffering only (though appearing as enjoyable to worldly-minded people). They have a beginning and an end (they come and go); Arjuna, it is for this reason that a wise man does not indulge in them."
+        ),
+        Verse(
+            verse_id = 23,
+            verse_name = "Verse 23",
+            verse = "शक्नोतीहैव यः सोढुं प्राक्शरीरविमोक्षणात्।\nकामक्रोधोद्भवं वेगं स युक्तः स सुखी नरः।।\n",
+            verse_meaning = "He alone, who is able to withstand, in this very life before casting off this body, the urges of lust and anger, is a Yogi, and he alone is a happy man."
+        ),
+        Verse(
+            verse_id = 24,
+            verse_name = "Verse 24",
+            verse = "योऽन्तःसुखोऽन्तरारामस्तथान्तर्ज्योतिरेव यः।\nस योगी ब्रह्मनिर्वाणं ब्रह्मभूतोऽधिगच्छति।।\n",
+            verse_meaning = "He who is happy within himself, enjoys within illumined by the inner light (light of the soul), such a Yogi (Sankhyayogi) identified with Brahma attains Brahma, who is all peace."
+        ),
+        Verse(
+            verse_id = 25,
+            verse_name = "Verse 25",
+            verse = "लभन्ते ब्रह्मनिर्वाणमृषयः क्षीणकल्मषाः।\nछिन्नद्वैधा यतात्मानः सर्वभूतहिते रताः।।\n",
+            verse_meaning = "The seers whose sins have been purged, whose doubts have been dispelled by knowledge, whose disciplined mind is firmly established in God and who are devoted to the welfare of all beings, attain Brahma, who is all peace."
+        ),
+        Verse(
+            verse_id = 26,
+            verse_name = "Verse 26",
+            verse = "कामक्रोधवियुक्तानां यतीनां यतचेतसाम्।\nअभितो ब्रह्मनिर्वाणं वर्तते विदितात्मनाम्।।\n",
+            verse_meaning = "To those wise men who are free from lust and anger, who have subdued their mind and have realized God, Brahma, the abode of eternal peace, is present all-round."
+        ),
+        Verse(
+            verse_id = 27,
+            verse_name = "Verse 27",
+            verse = "स्पर्शान्कृत्वा बहिर्बाह्यांश्चक्षुश्चैवान्तरे भ्रुवोः।\nप्राणापानौ समौ कृत्वा नासाभ्यन्तरचारिणौ।।\n",
+            verse_meaning = "Continued on Verse 28"
+        ),
+        Verse(
+            verse_id = 28,
+            verse_name = "Verse 28",
+            verse = "यतेन्द्रियमनोबुद्धिर्मुनिर्मोक्षपरायणः।\nविगतेच्छाभयक्रोधो यः सदा मुक्त एव सः।।\n",
+            verse_meaning = "Shutting out all thoughts of external enjoyments, with the gaze fixed on the space between the eye-brows, having regulated the Prana (outgoing) and the Apana (incoming) breaths flowing within the nostrils, he who has brought his senses, mind and intellect under control - such a contemplative soul intent on liberation and free from desire, fear and anger, is ever liberated. (27 and 28)"
+        ),
+        Verse(
+            verse_id = 29,
+            verse_name = "Verse 29",
+            verse = "भोक्तारं यज्ञतपसां सर्वलोकमहेश्वरम्।\nसुहृदं सर्वभूतानां ज्ञात्वा मां शान्तिमृच्छति।।\n",
+            verse_meaning = "Having known Me in reality as the enjoyer of all sacrifices and austerities, the supreme Lord of all the worlds, and the selfless friend of all beings, My devotee attains peace."
         )
     )
 }
