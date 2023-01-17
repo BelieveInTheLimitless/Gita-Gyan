88d7eff212cb8a753a86b16f89fd3a1a5bedcfa7
Vishal Patil <believeinthelimitless@gmail.com>
2023-01-17 11:19:58 +0530

    Added English Chapter 10

--- app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter10.kt
+++ app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter10.kt
@@ -7,8 +7,254 @@ fun getEnglishChapter10(): List<Verse>{
         Verse(
             verse_id = 1,
             verse_name = "Verse 1",
-            verse = "श्री भगवानुवाच\n\nभूय एव महाबाहो श्रृणु मे परमं वचः।\nयत्तेऽहं प्रीयमाणाय वक्ष्यामि हितकाम्यया।।10.1।।\n",
+            verse = "श्री भगवानुवाच\n\nभूय एव महाबाहो श्रृणु मे परमं वचः।\nयत्तेऽहं प्रीयमाणाय वक्ष्यामि हितकाम्यया।।\n",
             verse_meaning = "Shri Bhagavan said : Arjuna, hear once again My supreme word, which I shall speak to you, who are so loving, out of solicitude for your welfare."
+        ),
+        Verse(
+            verse_id = 2,
+            verse_name = "Verse 2",
+            verse = "न मे विदुः सुरगणाः प्रभवं न महर्षयः।\nअहमादिर्हि देवानां महर्षीणां च सर्वशः।।\n",
+            verse_meaning = "Neither gods nor the great sages know the secret of My birth (i.e., My appearance in human or other garb out of mere sport); for I am the prime cause in all respects of gods as well as of the great seers."
+        ),
+        Verse(
+            verse_id = 3,
+            verse_name = "Verse 3",
+            verse = "यो मामजमनादिं च वेत्ति लोकमहेश्वरम्।\nअसम्मूढः स मर्त्येषु सर्वपापैः प्रमुच्यते।।\n",
+            verse_meaning = "He who knows Me in reality as unborn and without beginning, and as the supreme Lord of the Universe, he, undeluded among men, is purged of all sins."
+        ),
+        Verse(
+            verse_id = 4,
+            verse_name = "Verse 4",
+            verse = "बुद्धिर्ज्ञानमसंमोहः क्षमा सत्यं दमः शमः।\nसुखं दुःखं भवोऽभावो भयं चाभयमेव च।।\n",
+            verse_meaning = "Continued on Verse 5"
+        ),
+        Verse(
+            verse_id = 5,
+            verse_name = "Verse 5",
+            verse = "अहिंसा समता तुष्टिस्तपो दानं यशोऽयशः।\nभवन्ति भावा भूतानां मत्त एव पृथग्विधाः।।\n",
+            verse_meaning = "Reason, right knowledge, unclouded understanding, forbearance, veracity, control over the senses and mind, joy and sorrow, evolution and dissolution, fear and fearlessness, non-violence, equanimity, contentment, austerity, charity, fame and obloquy - these diverse traits of creatures emanate from Me alone. (4 and 5)"
+        ),
+        Verse(
+            verse_id = 6,
+            verse_name = "Verse 6",
+            verse = "महर्षयः सप्त पूर्वे चत्वारो मनवस्तथा।\nमद्भावा मानसा जाता येषां लोक इमाः प्रजाः।।\n",
+            verse_meaning = "The seven great seers, their four elders (Sanaka and others), and the fourteen Manus or progenitors of mankind (such as Svayambhuva and his successors), who are all devoted to Me, were born of My will; to whom all these creatures belong."
+        ),
+        Verse(
+            verse_id = 7,
+            verse_name = "Verse 7",
+            verse = "एतां विभूतिं योगं च मम यो वेत्ति तत्त्वतः।\nसोऽविकम्पेन योगेन युज्यते नात्र संशयः।।\n",
+            verse_meaning = "He who knows in reality this supreme divine glory and supernatural power of Mine gets established in Me through unfaltering devotion; of this there is no doubt."
+        ),
+        Verse(
+            verse_id = 8,
+            verse_name = "Verse 8",
+            verse = "अहं सर्वस्य प्रभवो मत्तः सर्वं प्रवर्तते।\nइति मत्वा भजन्ते मां बुधा भावसमन्विताः।।\n",
+            verse_meaning = "I am the source of all creation and everything in the world moves because of Me; knowing thus, the wise, full of devotion, constantly worship Me."
+        ),
+        Verse(
+            verse_id = 9,
+            verse_name = "Verse 9",
+            verse = "मच्चित्ता मद्गतप्राणा बोधयन्तः परस्परम्।\nकथयन्तश्च मां नित्यं तुष्यन्ति च रमन्ति च।।\n",
+            verse_meaning = "With their minds fixed on Me, and their lives surrendered to Me, conversing and enlightening one another about My glories, My devotees ever remain contented and take delight in Me."
+        ),
+        Verse(
+            verse_id = 10,
+            verse_name = "Verse 10",
+            verse = "तेषां सततयुक्तानां भजतां प्रीतिपूर्वकम्।\nददामि बुद्धियोगं तं येन मामुपयान्ति ते।।\n",
+            verse_meaning = "On those ever united through meditation with Me and worshipping Me with love, I confer that Yoga of wisdom through which they come to Me."
+        ),
+        Verse(
+            verse_id = 11,
+            verse_name = "Verse 11",
+            verse = "तेषामेवानुकम्पार्थमहमज्ञानजं तमः।\nनाशयाम्यात्मभावस्थो ज्ञानदीपेन भास्वता।।\n",
+            verse_meaning = "In order to bestow My compassion on them, I, dwelling in their hearts, dispel their darkness born of ignorance by the illuminating lamp of knowledge."
+        ),
+        Verse(
+            verse_id = 12,
+            verse_name = "Verse 12",
+            verse = "अर्जुन उवाच\n\nपरं ब्रह्म परं धाम पवित्रं परमं भवान्।\nपुरुषं शाश्वतं दिव्यमादिदेवमजं विभुम्।।\n",
+            verse_meaning = "Continued on Verse 13"
+        ),
+        Verse(
+            verse_id = 13,
+            verse_name = "Verse 13",
+            verse = "आहुस्त्वामृषयः सर्वे देवर्षिर्नारदस्तथा।\nअसितो देवलो व्यासः स्वयं चैव ब्रवीषि मे।।\n",
+            verse_meaning = "Arjuna said : You are the transcendent Eternal, the supreme Abode and the greatest purifier; all the seers speak of You as the eternal divine Purusha, the primal Deity, unborn and all-pervading. Likewise speak the celestial sage Narada, the sages Asita and Devala and the great sage Vyasa; and Yourself too proclaim this to me. (12 and 13)"
+        ),
+        Verse(
+            verse_id = 14,
+            verse_name = "Verse 14",
+            verse = "सर्वमेतदृतं मन्ये यन्मां वदसि केशव।\nन हि ते भगवन् व्यक्ितं विदुर्देवा न दानवाः।।\n",
+            verse_meaning = "Krishna, I believe as true all that You tell me. Lord, neither demons nor gods are aware of Your manifestations."
+        ),
+        Verse(
+            verse_id = 15,
+            verse_name = "Verse 15",
+            verse = "स्वयमेवात्मनाऽत्मानं वेत्थ त्वं पुरुषोत्तम।\nभूतभावन भूतेश देवदेव जगत्पते।।\n",
+            verse_meaning = "O Creator of beings, O Ruler of creatures, god of gods, the Lord of the universe, O supreme Purusha, You alone know what You are by Yourself."
+        ),
+        Verse(
+            verse_id = 16,
+            verse_name = "Verse 16",
+            verse = "वक्तुमर्हस्यशेषेण दिव्या ह्यात्मविभूतयः।\nयाभिर्विभूतिभिर्लोकानिमांस्त्वं व्याप्य तिष्ठसि।।\n",
+            verse_meaning = "Therefore, You alone can describe in full Your divine glories, whereby You pervade all these worlds."
+        ),
+        Verse(
+            verse_id = 17,
+            verse_name = "Verse 17",
+            verse = "कथं विद्यामहं योगिंस्त्वां सदा परिचिन्तयन्।\nकेषु केषु च भावेषु चिन्त्योऽसि भगवन्मया।।\n",
+            verse_meaning = "O Master of Yoga, through what process of continuous meditation shall I know You? And in what particular forms, O Lord, are You to be meditated upon by me?"
+        ),
+        Verse(
+            verse_id = 18,
+            verse_name = "Verse 18",
+            verse = "विस्तरेणात्मनो योगं विभूतिं च जनार्दन।\nभूयः कथय तृप्तिर्हि श्रृण्वतो नास्ति मेऽमृतम्।।\n",
+            verse_meaning = "Krishna, tell me once more in detail Your power of Yoga and Your glory; for I know no satiety in hearing Your nectar-like words."
+        ),
+        Verse(
+            verse_id = 19,
+            verse_name = "Verse 19",
+            verse = "श्री भगवानुवाच\n\nहन्त ते कथयिष्यामि दिव्या ह्यात्मविभूतयः।\nप्राधान्यतः कुरुश्रेष्ठ नास्त्यन्तो विस्तरस्य मे।।\n",
+            verse_meaning = "Shri Bhagavan said : Arjuna, now I shall tell you My prominent divine glories; for there is no limit to My manifestations."
+        ),
+        Verse(
+            verse_id = 20,
+            verse_name = "Verse 20",
+            verse = "अहमात्मा गुडाकेश सर्वभूताशयस्थितः।\nअहमादिश्च मध्यं च भूतानामन्त एव च।।\n",
+            verse_meaning = "Arjuna, I am the universal Self seated in the hearts of all beings; so, I alone am the beginning, the middle and also the end of all beings."
+        ),
+        Verse(
+            verse_id = 21,
+            verse_name = "Verse 21",
+            verse = "आदित्यानामहं विष्णुर्ज्योतिषां रविरंशुमान्।\nमरीचिर्मरुतामस्मि नक्षत्राणामहं शशी।।\n",
+            verse_meaning = "I am Vishnu among the twelve sons of Aditi, and the radiant sun among the luminaries; I am the glow of the Maruts (the forty-nine wind-gods), and the moon the lord of the stars."
+        ),
+        Verse(
+            verse_id = 22,
+            verse_name = "Verse 22",
+            verse = "वेदानां सामवेदोऽस्मि देवानामस्मि वासवः।\nइन्द्रियाणां मनश्चास्मि भूतानामस्मि चेतना।।\n",
+            verse_meaning = "Among the Vedas, I am the Samaveda; among the gods, I am Indra. Among the organs of perception i.e., senses, I am the mind; and I am the consciousness (life-energy) in living beings."
+        ),
+        Verse(
+            verse_id = 23,
+            verse_name = "Verse 23",
+            verse = "रुद्राणां शङ्करश्चास्मि वित्तेशो यक्षरक्षसाम्।\nवसूनां पावकश्चास्मि मेरुः शिखरिणामहम्।।\n",
+            verse_meaning = "Among the eleven Rudras (gods of destruction), I am Shiva; and among the Yak¶as and Rakshasas, I am the lord of riches (Kubera). Among the eight Vasus, I am the god of fire; and among the mountains, I am the Meru."
+        ),
+        Verse(
+            verse_id = 24,
+            verse_name = "Verse 24",
+            verse = "पुरोधसां च मुख्यं मां विद्धि पार्थ बृहस्पतिम्।\nसेनानीनामहं स्कन्दः सरसामस्मि सागरः।।\n",
+            verse_meaning = "Among the priests, Arjuna, know Me to be their chief, Brhaspati. Among warrior-chiefs, I am Skanda (the generalissimo of the gods); and among the reservoirs of water, I am the ocean."
+        ),
+        Verse(
+            verse_id = 25,
+            verse_name = "Verse 25",
+            verse = "महर्षीणां भृगुरहं गिरामस्म्येकमक्षरम्।\nयज्ञानां जपयज्ञोऽस्मि स्थावराणां हिमालयः।।\n",
+            verse_meaning = "Among the great seers, I am Bhrgu; among words, I am the sacred syllable OÀ, among sacrifices, I am the sacrifice of Japa (muttering of sacred formulas); and among the immovables, the Himalayas."
+        ),
+        Verse(
+            verse_id = 26,
+            verse_name = "Verse 26",
+            verse = "अश्वत्थः सर्ववृक्षाणां देवर्षीणां च नारदः।\nगन्धर्वाणां चित्ररथः सिद्धानां कपिलो मुनिः।।\n",
+            verse_meaning = "Among all trees, I am the Ashvattha (the holy fig tree); among the celestial sages, Narada; among the Gandharvas (celestial musicians), Chitraratha, and among the Siddhas, I am the sage Kapila."
+        ),
+        Verse(
+            verse_id = 27,
+            verse_name = "Verse 27",
+            verse = "उच्चैःश्रवसमश्वानां विद्धि माममृतोद्भवम्।\nऐरावतं गजेन्द्राणां नराणां च नराधिपम्।।\n",
+            verse_meaning = "Among horses, know me to be the celestial horse Ucchaishrava, begotten of the churning of the ocean along with nectar; among mighty elephants, Airavata (Indra's elephant); and among men, the king."
+        ),
+        Verse(
+            verse_id = 28,
+            verse_name = "Verse 28",
+            verse = "आयुधानामहं वज्रं धेनूनामस्मि कामधुक्।\nप्रजनश्चास्मि कन्दर्पः सर्पाणामस्मि वासुकिः।।\n",
+            verse_meaning = "Among weapons, I am the thunderbolt; among cows, I am the celestial cow Kamadhenu (the cow of plenty). I am the sexual desire which leads to procreation (as enjoined by the scriptures); among serpents I am Vasuki."
+        ),
+        Verse(
+            verse_id = 29,
+            verse_name = "Verse 29",
+            verse = "अनन्तश्चास्मि नागानां वरुणो यादसामहम्।\nपितृ़णामर्यमा चास्मि यमः संयमतामहम्।।\n",
+            verse_meaning = "Among Nagas (a special class of serpents), I am the serpent-god Ananta; and I am Varuna, the lord of aquatic creatures. Among the manes, I am Aryama (the head of the Pitrs); and among rulers, I am Yama (the god of death)."
+        ),
+        Verse(
+            verse_id = 30,
+            verse_name = "Verse 30",
+            verse = "प्रह्लादश्चास्मि दैत्यानां कालः कलयतामहम्।\nमृगाणां च मृगेन्द्रोऽहं वैनतेयश्च पक्षिणाम्।।\n",
+            verse_meaning = "Among the Daityas, I am the great devotee Prahlada; and of calculators, I am Time; among quadrupeds, I am the lion; and among birds, I am Garuda."
+        ),
+        Verse(
+            verse_id = 31,
+            verse_name = "Verse 31",
+            verse = "पवनः पवतामस्मि रामः शस्त्रभृतामहम्।\nझषाणां मकरश्चास्मि स्रोतसामस्मि जाह्नवी।।\n",
+            verse_meaning = "Among purifiers, I am the wind; among warriors, I am Shri Rama. Among fishes, I am the shark; and among streams, I am the Ganges."
+        ),
+        Verse(
+            verse_id = 32,
+            verse_name = "Verse 32",
+            verse = "सर्गाणामादिरन्तश्च मध्यं चैवाहमर्जुन।\nअध्यात्मविद्या विद्यानां वादः प्रवदतामहम्।।\n",
+            verse_meaning = "Arjuna, I am the beginning, the middle and the end of all creations. Of all knowledge, I am the knowledge of the soul, (metaphysics); among disputants, I am the right type of reasoning."
+        ),
+        Verse(
+            verse_id = 33,
+            verse_name = "Verse 33",
+            verse = "अक्षराणामकारोऽस्मि द्वन्द्वः सामासिकस्य च।\nअहमेवाक्षयः कालो धाताऽहं विश्वतोमुखः।।\n",
+            verse_meaning = "Among the sounds represented by the various letters, I am 'A' (the sound represented by the first letter of the alphabet); of the different kinds of compounds in grammar, I am the copulative compound. I am verily the endless Time (the devourer of Time, God); I am the sustainer of all, having My face on all sides."
+        ),
+        Verse(
+            verse_id = 34,
+            verse_name = "Verse 34",
+            verse = "मृत्युः सर्वहरश्चाहमुद्भवश्च भविष्यताम्।\nकीर्तिः श्रीर्वाक्च नारीणां स्मृतिर्मेधा धृतिः क्षमा।।\n",
+            verse_meaning = "I am the all-destroying Death that annihilates all, and the origin of all that are to be born. Of feminities, I am Kirti, Shri,Vak, Smrti, Medha, Dhrti and Kshama (the goddesses presiding over glory, prosperity, speech, memory, intelligence, endurance and forbearance, respectively)."
+        ),
+        Verse(
+            verse_id = 35,
+            verse_name = "Verse 35",
+            verse = "बृहत्साम तथा साम्नां गायत्री छन्दसामहम्।\nमासानां मार्गशीर्षोऽहमृतूनां कुसुमाकरः।।\n",
+            verse_meaning = "Likewise, among the Shrutis that can be sung, I am the variety known as Brhatsama; while among the Vedic hymns, I am the hymn known as Gayatri. Again, among the twelve months of the Hindu calendar, I am the month known as 'Margashisha' (corresponding approximately to November December); and among the six seasons (successively appearing in India in the course of a year) I am the spring season."
+        ),
+        Verse(
+            verse_id = 36,
+            verse_name = "Verse 36",
+            verse = "द्यूतं छलयतामस्मि तेजस्तेजस्विनामहम्।\nजयोऽस्मि व्यवसायोऽस्मि सत्त्वं सत्त्ववतामहम्।।\n",
+            verse_meaning = "I am gambling among deceitful practices, and the glory of the glorious. I am the victory of the victorious, the resolve of the resolute, the goodness of the good."
+        ),
+        Verse(
+            verse_id = 37,
+            verse_name = "Verse 37",
+            verse = "वृष्णीनां वासुदेवोऽस्मि पाण्डवानां धनंजयः।\nमुनीनामप्यहं व्यासः कवीनामुशना कविः।।\n",
+            verse_meaning = "I am Krishna among the Vasnis, Arjuna among the sons of Pandu, Vyasa among the sages, and the sage Shukracharya among the wise."
+        ),
+        Verse(
+            verse_id = 38,
+            verse_name = "Verse 38",
+            verse = "दण्डो दमयतामस्मि नीतिरस्मि जिगीषताम्।\nमौनं चैवास्मि गुह्यानां ज्ञानं ज्ञानवतामहम्।।\n",
+            verse_meaning = "I am the subduing power of rulers; I am righteousness in those who seek to conquer. Of things to be kept secret, I am the custodian in the shape of reticence; and I am the wisdom of the wise."
+        ),
+        Verse(
+            verse_id = 39,
+            verse_name = "Verse 39",
+            verse = "यच्चापि सर्वभूतानां बीजं तदहमर्जुन\nन तदस्ति विना यत्स्यान्मया भूतं चराचरम्।।\n",
+            verse_meaning = "Arjuna, I am even that, which is the seed of all life. For there is no creature, moving or unmoving, which can exist without Me."
+        ),
+        Verse(
+            verse_id = 40,
+            verse_name = "Verse 40",
+            verse = "नान्तोऽस्ति मम दिव्यानां विभूतीनां परंतप।\nएष तूद्देशतः प्रोक्तो विभूतेर्विस्तरो मया।।\n",
+            verse_meaning = "Arjuna, there is no limit to My divine manifestations. This is only a brief description by Me of the extent of My glory."
+        ),
+        Verse(
+            verse_id = 41,
+            verse_name = "Verse 41",
+            verse = "यद्यद्विभूतिमत्सत्त्वं श्रीमदूर्जितमेव वा।\nतत्तदेवावगच्छ त्वं मम तेजोंऽशसंभवम्।।\n",
+            verse_meaning = "Every such being as is glorious, brilliant and powerful, know that to be a part manifestation of My glory."
+        ),
+        Verse(
+            verse_id = 42,
+            verse_name = "Verse 42",
+            verse = "अथवा बहुनैतेन किं ज्ञातेन तवार्जुन।\nविष्टभ्याहमिदं कृत्स्नमेकांशेन स्थितो जगत्।।\n",
+            verse_meaning = "Or, what will you gain by knowing all this in detail, Arjuna? Suffice it to say that I hold this entire universe by a fraction of My Yogic Power."
         )
     )
 }
