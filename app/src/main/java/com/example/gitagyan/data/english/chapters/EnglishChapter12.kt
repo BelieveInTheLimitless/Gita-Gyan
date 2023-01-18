ae0677848eb4e3fc2c0aaca24557dd327d53f22e
Vishal Patil <believeinthelimitless@gmail.com>
2023-01-18 22:47:54 +0530

    Added English Chapter 12

--- app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter12.kt
+++ app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter12.kt
@@ -7,8 +7,122 @@ fun getEnglishChapter12(): List<Verse>{
         Verse(
             verse_id = 1,
             verse_name = "Verse 1",
-            verse = "अर्जुन उवाच\n\nएवं सततयुक्ता ये भक्तास्त्वां पर्युपासते।\nयेचाप्यक्षरमव्यक्तं तेषां के योगवित्तमाः।।12.1।।\n",
-            verse_meaning = "Arjuna said : The devotees exclusively and constantly devoted to You in the manner stated just earlier, adore You as possessed of form and attributes, and those who adore as the supreme Reality only the indestructible unmanifest Brahma (who is Truth, Knowledge and Bliss solidified) of these two types of worshippers who are the best knowers of Yoga?"
+            verse = "अर्जुन उवाच\n\nएवं सततयुक्ता ये भक्तास्त्वां पर्युपासते।\nयेचाप्यक्षरमव्यक्तं तेषां के योगवित्तमाः।।\n",
+            verse_meaning = "Arjuna said : The devotees exclusively and constantly devoted to You in the manner stated just earlier, adore You as possessed of form and attributes, and those who adore as the supreme Reality only the indestructible unmanifest Brahma (who is Truth, Knowledge and Bliss solidified) - of these two types of worshippers who are the best knowers of Yoga?"
+        ),
+        Verse(
+            verse_id = 2,
+            verse_name = "Verse 2",
+            verse = "श्री भगवानुवाच\n\nमय्यावेश्य मनो ये मां नित्ययुक्ता उपासते।\nश्रद्धया परयोपेतास्ते मे युक्ततमा मताः।।\n",
+            verse_meaning = "Shri Bhagavan said : I consider them to be the best Yogis, who endowed with supreme faith, and ever united through meditation with Me, worship Me with their mind centred on Me."
+        ),
+        Verse(
+            verse_id = 3,
+            verse_name = "Verse 3",
+            verse = "ये त्वक्षरमनिर्देश्यमव्यक्तं पर्युपासते।\nसर्वत्रगमचिन्त्यं च कूटस्थमचलं ध्रुवम्।।\n",
+            verse_meaning = "Continued on Verse 4"
+        ),
+        Verse(
+            verse_id = 4,
+            verse_name = "Verse 4",
+            verse = "संनियम्येन्द्रियग्रामं सर्वत्र समबुद्धयः।\nते प्राप्नुवन्ति मामेव सर्वभूतहिते रताः।।\n",
+            verse_meaning = "Those, however, who fully controlling all their senses and even-minded towards all, and devoted to the welfare of all beings, constantly adore as their very self the unthinkable, omnipresent, indestructible, indefinable, eternal, immovable, unmanifest and changeless Brahma, they too come to Me. (3 and 4)"
+        ),
+        Verse(
+            verse_id = 5,
+            verse_name = "Verse 5",
+            verse = "क्लेशोऽधिकतरस्तेषामव्यक्तासक्तचेतसाम्।\nअव्यक्ता हि गतिर्दुःखं देहवद्भिरवाप्यते।।\n",
+            verse_meaning = "Of course, the strain is greater for those who have their mind attached to the Unmanifest, as attunement with the Unmanifest is attained with difficulty by the body-conscious people."
+        ),
+        Verse(
+            verse_id = 6,
+            verse_name = "Verse 6",
+            verse = "ये तु सर्वाणि कर्माणि मयि संन्यस्य मत्पराः।\nअनन्येनैव योगेन मां ध्यायन्त उपासते।।\n",
+            verse_meaning = "Continued on Verse 7"
+        ),
+        Verse(
+            verse_id = 7,
+            verse_name = "Verse 7",
+            verse = "तेषामहं समुद्धर्ता मृत्युसंसारसागरात्।\nभवामि नचिरात्पार्थ मय्यावेशितचेतसाम्।।\n",
+            verse_meaning = "On the other hand, those depending exclusively on Me, and surrendering all actions to Me, worship Me (God with attributes), constantly meditating on Me with single-minded devotion, them, Arjuna, I speedily deliver from the ocean of birth and death, their mind being fixed on Me. (6 and 7)"
+        ),
+        Verse(
+            verse_id = 8,
+            verse_name = "Verse 8",
+            verse = "मय्येव मन आधत्स्व मयि बुद्धिं निवेशय।\nनिवसिष्यसि मय्येव अत ऊर्ध्वं न संशयः।।\n",
+            verse_meaning = "Therefore, fix your mind on Me, and establish your intellect in Me alone; thereafter you will abide solely in Me. There is no doubt about it."
+        ),
+        Verse(
+            verse_id = 9,
+            verse_name = "Verse 9",
+            verse = "अथ चित्तं समाधातुं न शक्नोषि मयि स्थिरम्।\nअभ्यासयोगेन ततो मामिच्छाप्तुं धनञ्जय।।\n",
+            verse_meaning = "If you cannot steadily fix the mind on Me, Arjuna, then seek to attain Me through the Yoga of practice."
+        ),
+        Verse(
+            verse_id = 10,
+            verse_name = "Verse 10",
+            verse = "अभ्यासेऽप्यसमर्थोऽसि मत्कर्मपरमो भव।\nमदर्थमपि कर्माणि कुर्वन् सिद्धिमवाप्स्यसि।।\n",
+            verse_meaning = "If you are unequal even to the pursuit of such practice, be intent to work for Me; you shall attain perfection (in the shape of My realization) even by performing actions for My sake."
+        ),
+        Verse(
+            verse_id = 11,
+            verse_name = "Verse 11",
+            verse = "अथैतदप्यशक्तोऽसि कर्तुं मद्योगमाश्रितः।\nसर्वकर्मफलत्यागं ततः कुरु यतात्मवान्।।\n",
+            verse_meaning = "If, taking recourse to the Yoga of My realization, you are unable even to do this, then, subduing your mind and intellect etc., relinquish the fruit of all actions."
+        ),
+        Verse(
+            verse_id = 12,
+            verse_name = "Verse 12",
+            verse = "श्रेयो हि ज्ञानमभ्यासाज्ज्ञानाद्ध्यानं विशिष्यते।\nध्यानात्कर्मफलत्यागस्त्यागाच्छान्तिरनन्तरम्।।\n",
+            verse_meaning = "Knowledge is better than practice without discernment, meditation on God is superior to knowledge, and renunciation of the fruit of actions is even superior to meditation; for, peace immediately follows from renunciation."
+        ),
+        Verse(
+            verse_id = 13,
+            verse_name = "Verse 13",
+            verse = "अद्वेष्टा सर्वभूतानां मैत्रः करुण एव च।\nनिर्ममो निरहङ्कारः समदुःखसुखः क्षमी।।\n",
+            verse_meaning = "Continued on Verse 14"
+        ),
+        Verse(
+            verse_id = 14,
+            verse_name = "Verse 14",
+            verse = "सन्तुष्टः सततं योगी यतात्मा दृढनिश्चयः।\nमय्यर्पितमनोबुद्धिर्यो मद्भक्तः स मे प्रियः।।\n",
+            verse_meaning = "He who is free from malice towards all beings, friendly and compassionate, and free from the feelings of 'I' and 'mine', balanced in joy and sorrow, forgiving by nature, ever-contented and mentally united with Me, nay, who has subdued his mind, senses and body, has a firm resolve, and has surrendered his mind and reason to Me - that devotee of Mine is dear to Me. (13 and 14)"
+        ),
+        Verse(
+            verse_id = 15,
+            verse_name = "Verse 15",
+            verse = "यस्मान्नोद्विजते लोको लोकान्नोद्विजते च यः।\nहर्षामर्षभयोद्वेगैर्मुक्तो यः स च मे प्रियः।।\n",
+            verse_meaning = "He who is not a source of annoyance to his fellow-creatures, and who in his turn does not feel vexed with his fellow-creatures, and who is free from delight and envy, perturbation and fear, is dear to Me."
+        ),
+        Verse(
+            verse_id = 16,
+            verse_name = "Verse 16",
+            verse = "अनपेक्षः शुचिर्दक्ष उदासीनो गतव्यथः।\nसर्वारम्भपरित्यागी यो मद्भक्तः स मे प्रियः।।\n",
+            verse_meaning = "He who wants nothing, who is both internally and externally pure, is wise and impartial and has risen above all distractions, and who renounces the sense of doership in all undertakings - such a devotee of Mine is dear to Me."
+        ),
+        Verse(
+            verse_id = 17,
+            verse_name = "Verse 17",
+            verse = "यो न हृष्यति न द्वेष्टि न शोचति न काङ्क्षति।\nशुभाशुभपरित्यागी भक्ितमान्यः स मे प्रियः।।\n",
+            verse_meaning = "He who neither rejoices nor hates, nor grieves, nor desires, and who renounces both good and evil actions and is full of devotion, is dear to Me."
+        ),
+        Verse(
+            verse_id = 18,
+            verse_name = "Verse 18",
+            verse = "समः शत्रौ च मित्रे च तथा मानापमानयोः।\nशीतोष्णसुखदुःखेषु समः सङ्गविवर्जितः।।\n",
+            verse_meaning = "Continued on Verse 19"
+        ),
+        Verse(
+            verse_id = 19,
+            verse_name = "Verse 19",
+            verse = "तुल्यनिन्दास्तुतिर्मौनी सन्तुष्टो येनकेनचित्।\nअनिकेतः स्थिरमतिर्भक्ितमान्मे प्रियो नरः।।\n",
+            verse_meaning = "He who deals equally with friend and foe, and is the same in honour and ignominy, who is alike in heat and cold, pleasure and pain and other contrary experiences, and is free from attachment, he who takes praise and reproach alike, and is given to contemplation and is contented with any means of subsistence available, entertaining no sense of ownership and attachment in respect of his dwelling-place and is full of devotion to Me, that person is dear to Me. (18 and 19)"
+        ),
+        Verse(
+            verse_id = 20,
+            verse_name = "Verse 20",
+            verse = "ये तु धर्म्यामृतमिदं यथोक्तं पर्युपासते।\nश्रद्दधाना मत्परमा भक्तास्तेऽतीव मे प्रियाः।।\n",
+            verse_meaning = "Those devotees, however, who partake in a disinterested way of this nectar of pious wisdom set forth above, endowed with faith and solely devoted to Me, they are extremely dear to Me."
         )
     )
 }
