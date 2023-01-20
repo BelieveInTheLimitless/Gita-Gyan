52727e9e8656680b96a9a816ba1138ed9d5dd37d
Vishal Patil <believeinthelimitless@gmail.com>
2023-01-20 22:12:11 +0530

    Added English Chapter 17

--- app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter17.kt
+++ app/src/main/java/com/example/gitagyan/data/english/chapters/EnglishChapter17.kt
@@ -7,8 +7,170 @@ fun getEnglishChapter17(): List<Verse>{
         Verse(
             verse_id = 1,
             verse_name = "Verse 1",
-            verse = "अर्जुन उवाच\n\nये शास्त्रविधिमुत्सृज्य यजन्ते श्रद्धयाऽन्विताः।\nतेषां निष्ठा तु का कृष्ण सत्त्वमाहो रजस्तमः।।17.1।।\n",
+            verse = "अर्जुन उवाच\n\nये शास्त्रविधिमुत्सृज्य यजन्ते श्रद्धयाऽन्विताः।\nतेषां निष्ठा तु का कृष्ण सत्त्वमाहो रजस्तमः।।\n",
             verse_meaning = "Arjuna said: Those, endowed with faith, who worship gods and others, disregarding the injunctions of the scriptures, where do they stand, Krishna - in Sattva, Rajas or Tamas ?"
+        ),
+        Verse(
+            verse_id = 2,
+            verse_name = "Verse 2",
+            verse = "श्री भगवानुवाच\n\nत्रिविधा भवति श्रद्धा देहिनां सा स्वभावजा।\nसात्त्विकी राजसी चैव तामसी चेति तां श्रृणु।।\n",
+            verse_meaning = "Shri Bhagavan said: That untutored innate faith of men is of three kinds - Sattvika, Rajasika and Tamasika. Hear of it from Me.",
+            ),
+        Verse(
+            verse_id = 3,
+            verse_name = "Verse 3",
+            verse = "सत्त्वानुरूपा सर्वस्य श्रद्धा भवति भारत।\nश्रद्धामयोऽयं पुरुषो यो यच्छ्रद्धः स एव सः।।\n",
+            verse_meaning = "The faith of all men conforms to their mental disposition, Arjuna. Faith constitutes a man; whatever the nature of his faith, verily he is that."
+        ),
+        Verse(
+            verse_id = 4,
+            verse_name = "Verse 4",
+            verse = "यजन्ते सात्त्विका देवान्यक्षरक्षांसि राजसाः।\nप्रेतान्भूतगणांश्चान्ये यजन्ते तामसा जनाः।।\n",
+            verse_meaning = "Men of Sattvika disposition worship gods; those of Rajasika temperament worship demigods, the demons; while others, who are of Tamasika disposition, worship the spirits of the dead and ghosts."
+        ),
+        Verse(
+            verse_id = 5,
+            verse_name = "Verse 5",
+            verse = "अशास्त्रविहितं घोरं तप्यन्ते ये तपो जनाः।\nदम्भाहङ्कारसंयुक्ताः कामरागबलान्विताः।।\n",
+            verse_meaning = "Men who practise severe penance of an arbitrary type, not sanctioned by the scriptures, and who are full of hypocrisy and egotism and are obsessed with desire, attachment and pride of power;"
+        ),
+        Verse(
+            verse_id = 6,
+            verse_name = "Verse 6",
+            verse = "कर्षयन्तः शरीरस्थं भूतग्राममचेतसः।\nमां चैवान्तःशरीरस्थं तान्विद्ध्यासुरनिश्चयान्।।\n",
+            verse_meaning = "And who emaciate the elements constituting their body as well as Me, the Supreme Spirit, dwelling in their heart - know those senseless people to have a demoniac disposition."
+        ),
+        Verse(
+            verse_id = 7,
+            verse_name = "Verse 7",
+            verse = "आहारस्त्वपि सर्वस्य त्रिविधो भवति प्रियः।\nयज्ञस्तपस्तथा दानं तेषां भेदमिमं श्रृणु।।\n",
+            verse_meaning = "Food also, which is agreeable to different men according to their innate disposition is of three kinds. And likewise, sacrifice, penance and charity too are of three kinds each; hear their distinction as follows."
+        ),
+        Verse(
+            verse_id = 8,
+            verse_name = "Verse 8",
+            verse = "आयुःसत्त्वबलारोग्यसुखप्रीतिविवर्धनाः।\nरस्याः स्निग्धाः स्थिरा हृद्या आहाराः सात्त्विकप्रियाः।।\n",
+            verse_meaning = "Foods which promote longevity, intelligence, vigour, health, happiness and cheerfulness, and which are juicy, succulent, substantial and naturally agreeable, are liked by men of Sattvika nature."
+        ),
+        Verse(
+            verse_id = 9,
+            verse_name = "Verse 9",
+            verse = "कट्वम्ललवणात्युष्णतीक्ष्णरूक्षविदाहिनः।\nआहारा राजसस्येष्टा दुःखशोकामयप्रदाः।।\n",
+            verse_meaning = "Foods which are bitter, sour, salty, overhot, pungent, dry and burning, and which cause suffering, grief and sickness, are dear to the Rajasika."
+        ),
+        Verse(
+            verse_id = 10,
+            verse_name = "Verse 10",
+            verse = "यातयामं गतरसं पूति पर्युषितं च यत्।\nउच्छिष्टमपि चामेध्यं भोजनं तामसप्रियम्।।\n",
+            verse_meaning = "Food which is ill-cooked or not fully ripe, insipid, putrid, stale and polluted, and which is impure too, is dear to men of Tamasika disposition."
+        ),
+        Verse(
+            verse_id = 11,
+            verse_name = "Verse 11",
+            verse = "अफलाकाङ्क्षिभिर्यज्ञो विधिदृष्टो य इज्यते।\nयष्टव्यमेवेति मनः समाधाय स सात्त्विकः।।\n",
+            verse_meaning = "The sacrifice which is offered, as ordained by scriptural injunctions, by men who expect no return and who believe that such sacrifices must be performed, is Sattvika in character."
+        ),
+        Verse(
+            verse_id = 12,
+            verse_name = "Verse 12",
+            verse = "अभिसंधाय तु फलं दम्भार्थमपि चैव यत्।\nइज्यते भरतश्रेष्ठ तं यज्ञं विद्धि राजसम्।।\n",
+            verse_meaning = "That sacrifice, however, which is offered for the sake of mere show or even with an eye to its fruit, know it to be Rajasika, Arjuna."
+        ),
+        Verse(
+            verse_id = 13,
+            verse_name = "Verse 13",
+            verse = "विधिहीनमसृष्टान्नं मन्त्रहीनमदक्षिणम्।\nश्रद्धाविरहितं यज्ञं तामसं परिचक्षते।।\n",
+            verse_meaning = "A sacrifice, which is not in conformity with scriptural injunctions, in which no food is offered, and no sacrificial fees are paid, which is without sacred chant of hymns and devoid of faith, is said to be Tamasika."
+        ),
+        Verse(
+            verse_id = 14,
+            verse_name = "Verse 14",
+            verse = "देवद्विजगुरुप्राज्ञपूजनं शौचमार्जवम्।\nब्रह्मचर्यमहिंसा च शारीरं तप उच्यते।।\n",
+            verse_meaning = "Worship of gods, the Brahmanas, one's guru, elders and wise-men, purity, straightforwardness, continence and non-violence - these are called penance of the body."
+        ),
+        Verse(
+            verse_id = 15,
+            verse_name = "Verse 15",
+            verse = "अनुद्वेगकरं वाक्यं सत्यं प्रियहितं च यत्।\nस्वाध्यायाभ्यसनं चैव वाङ्मयं तप उच्यते।।\n",
+            verse_meaning = "Words which cause no annoyance to others and are truthful, agreeable and beneficial, as well as the study of the Vedas and other Shstras and the practice of the chanting of Divine Name - this is known as penance of speech."
+        ),
+        Verse(
+            verse_id = 16,
+            verse_name = "Verse 16",
+            verse = "मनःप्रसादः सौम्यत्वं मौनमात्मविनिग्रहः।\nभावसंशुद्धिरित्येतत्तपो मानसमुच्यते।।\n",
+            verse_meaning = "Cheerfulness of mind, placidity, habit of contemplation on God, control of the mind and perfect purity of inner feelings - all this is called austerity of the mind."
+        ),
+        Verse(
+            verse_id = 17,
+            verse_name = "Verse 17",
+            verse = "श्रद्धया परया तप्तं तपस्तत्ित्रविधं नरैः।\nअफलाकाङ्क्षिभिर्युक्तैः सात्त्विकं परिचक्षते।।\n",
+            verse_meaning = "This threefold penance performed with supreme faith by Yog∂s expecting no return is called Sattvika."
+        ),
+        Verse(
+            verse_id = 18,
+            verse_name = "Verse 18",
+            verse = "सत्कारमानपूजार्थं तपो दम्भेन चैव यत्।\nक्रियते तदिह प्रोक्तं राजसं चलमध्रुवम्।।\n",
+            verse_meaning = "The austerity which is performed for the sake of renown, honour or adoration, as well as for any other selfish gain, either in all sincerity or by way of ostentation, and yields an uncertain and momentary fruit, has been spoken of here as Rajasika."
+        ),
+        Verse(
+            verse_id = 19,
+            verse_name = "Verse 19",
+            verse = "मूढग्राहेणात्मनो यत्पीडया क्रियते तपः।\nपरस्योत्सादनार्थं वा तत्तामसमुदाहृतम्।।\n",
+            verse_meaning = "Penance which is resorted to out of foolish notion and is accompanied by self-mortification, or is intended to harm others, such penance has been declared as Tamasika."
+        ),
+        Verse(
+            verse_id = 20,
+            verse_name = "Verse 20",
+            verse = "दातव्यमिति यद्दानं दीयतेऽनुपकारिणे।\nदेशे काले च पात्रे च तद्दानं सात्त्विकं स्मृतम्।।\n",
+            verse_meaning = "A gift which is bestowed with a sense of duty on one from whom no return is expected, at appropriate time and place, and to a deserving person, that gift has been declared as Sattvika."
+        ),
+        Verse(
+            verse_id = 21,
+            verse_name = "Verse 21",
+            verse = "यत्तु प्रत्युपकारार्थं फलमुद्दिश्य वा पुनः।\nदीयते च परिक्लिष्टं तद्दानं राजसं स्मृतम्।।\n",
+            verse_meaning = "A gift which is bestowed in a grudging spirit and with the object of getting a service in return or in the hope of obtaining a reward, is called Rajasika."
+        ),
+        Verse(
+            verse_id = 22,
+            verse_name = "Verse 22",
+            verse = "अदेशकाले यद्दानमपात्रेभ्यश्च दीयते।\nअसत्कृतमवज्ञातं तत्तामसमुदाहृतम्।।\n",
+            verse_meaning = "A gift which is made without good grace and in a disdainful spirit, out of time and place, and to undeserving persons, is said to be Tamasika."
+        ),
+        Verse(
+            verse_id = 23,
+            verse_name = "Verse 23",
+            verse = "तत्सदिति निर्देशो ब्रह्मणस्त्रिविधः स्मृतः।\nब्राह्मणास्तेन वेदाश्च यज्ञाश्च विहिताः पुरा।।\n",
+            verse_meaning = "OM, TAT and SAT - this has been declared as the triple appellation of Brahma, who is Truth, Consciousness and Bliss. By that were the Brahmanas and the Vedas as well as sacrifices created at the cosmic dawn."
+        ),
+        Verse(
+            verse_id = 24,
+            verse_name = "Verse 24",
+            verse = "तस्मादोमित्युदाहृत्य यज्ञदानतपःक्रियाः।\nप्रवर्तन्ते विधानोक्ताः सततं ब्रह्मवादिनाम्।।\n",
+            verse_meaning = "Therefore, acts of sacrifice, charity and austerity, as enjoined by sacred precepts, are always commenced by noble persons, used to the recitation of Vedic chants, with the invocation of the divine name 'OM'."
+        ),
+        Verse(
+            verse_id = 25,
+            verse_name = "Verse 25",
+            verse = "तदित्यनभिसन्धाय फलं यज्ञतपःक्रियाः।\nदानक्रियाश्च विविधाः क्रियन्ते मोक्षकाङ्क्षि।।\n",
+            verse_meaning = "With the idea that all this belongs to God, who is denoted by the appellation TAT, acts of sacrifice and austerity as well as acts of charity of various kinds, are performed by the seekers of liberation, expecting no return for them."
+        ),
+        Verse(
+            verse_id = 26,
+            verse_name = "Verse 26",
+            verse = "सद्भावे साधुभावे च सदित्येतत्प्रयुज्यते।\nप्रशस्ते कर्मणि तथा सच्छब्दः पार्थ युज्यते।।\n",
+            verse_meaning = "The name of God, 'SAT', is used in the sense of reality and goodness. And the word 'SAT' is also used in the sense of a praiseworthy, auspicious action, Arjuna."
+        ),
+        Verse(
+            verse_id = 27,
+            verse_name = "Verse 27",
+            verse = "यज्ञे तपसि दाने च स्थितिः सदिति चोच्यते।\nकर्म चैव तदर्थीयं सदित्येवाभिधीयते।।\n",
+            verse_meaning = "And steadfastness in sacrifice, austerity and charity is likewise spoken of as 'SAT' and action for the sake of God is verily termed as 'SAT'."
+        ),
+        Verse(
+            verse_id = 28,
+            verse_name = "Verse 28",
+            verse = "अश्रद्धया हुतं दत्तं तपस्तप्तं कृतं च यत्।\nअसदित्युच्यते पार्थ न च तत्प्रेत्य नो इह।।\n",
+            verse_meaning = "An oblation which is offered, a gift given, an austerity practised, and whatever good deed is performed, if it is without faith, it is termed as naught i.e., 'asat'; therefore, it is of no avail here or hereafter."
         )
     )
 }
