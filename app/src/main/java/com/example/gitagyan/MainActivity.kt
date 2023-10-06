588368f726b63b70ebacbd37d2146035e814f1f2
Vishal Patil <believeinthelimitless@gmail.com>
2023-10-06 21:57:13 +0530

    Added favourite icon in VerseScreen to add favourite verses

--- app/src/main/java/com/example/gitagyan/MainActivity.kt
+++ app/src/main/java/com/example/gitagyan/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.gitagyan.navigation.AppNavigation
 import com.example.gitagyan.ui.theme.GitaGyanTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/gitagyan/screens/home/VerseScreen.kt
+++ app/src/main/java/com/example/gitagyan/screens/home/VerseScreen.kt
@@ -1,5 +1,7 @@
 package com.example.gitagyan.screens.home
 
+import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.clickable
@@ -11,68 +13,90 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.gitagyan.R
-import com.example.gitagyan.data.Chapter
+import com.example.gitagyan.data.english.Chapter
 import com.example.gitagyan.data.english.getEnglishChapters
+import com.example.gitagyan.model.Favourite
 import com.example.gitagyan.screens.components.topbar.TopBottomBar
+import com.example.gitagyan.screens.favourite.FavouriteViewModel
 
 @Composable
-fun VerseScreen(navController: NavController, chapter_id: String?, verse_id: String?) {
+fun VerseScreen(navController: NavController, favouriteViewModel: FavouriteViewModel = hiltViewModel(), chapterId: String?, verseId: String?) {
     val chapters = getEnglishChapters()
     TopBottomBar(navController = navController)
     Box(modifier = Modifier.padding(top = 60.dp, bottom = 60.dp)) {
-        for (chapter in chapters) {
-            if (chapter_id == chapter.chapter_id) {
-                if (verse_id != null) {
-                    Verses(
-                        chapter = chapter,
-                        verseId = verse_id
-                    )
-                }
+        if (chapterId != null) {
+            if (verseId != null) {
+                Verses(favouriteViewModel, chapter = chapters[chapterId.toInt()],
+                    verseId = verseId
+                )
             }
         }
     }
 }
 
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun Verses(chapter: Chapter, verseId: String){
+fun Verses(favouriteViewModel: FavouriteViewModel = hiltViewModel(), chapter: Chapter, verseId: String) {
+
     var id by remember {
-        mutableStateOf(verseId.toInt())
+        mutableIntStateOf(verseId.toInt())
     }
-    Surface(modifier = Modifier.fillMaxSize(),
-        color = Color(0xFFFD950E)) {
-        Surface(modifier = Modifier
-            .padding(15.dp)
-            .fillMaxSize(),
+
+    val isAlreadyFavourite = favouriteViewModel.favList.collectAsState().value.filter { item ->
+        (item.chapterId == chapter.chapterId) && ((item.verseId.toInt() - 1) == id)
+    }
+
+    val context = LocalContext.current
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFFD950E)
+    ) {
+        Surface(
+            modifier = Modifier
+                .padding(15.dp)
+                .fillMaxSize(),
             shape = RoundedCornerShape(corner = CornerSize(40.dp)),
             color = Color.White,
-            contentColor = Color.Black) {
-            Row(Modifier.padding(top = 45.dp, bottom = 45.dp),
-                horizontalArrangement = Arrangement.Center) {
-                Column(modifier = Modifier.padding(10.dp),
+            contentColor = Color.Black
+        ) {
+            Row(
+                Modifier.padding(top = 45.dp, bottom = 45.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Column(
+                    modifier = Modifier.padding(10.dp),
                     verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
-                    Image(painter = painterResource(id = R.drawable.krishna_arjuna),
+                    Image(
+                        painter = painterResource(id = R.drawable.krishna_arjuna),
                         contentDescription = null,
-                        contentScale = ContentScale.FillBounds)
+                        contentScale = ContentScale.FillBounds
+                    )
 
                     Spacer(modifier = Modifier.padding(5.dp))
 
                     Text(
-                        text = chapter.chapter_name,
+                        text = chapter.chapterName,
                         modifier = Modifier.padding(top = 10.dp),
                         fontSize = 20.sp,
                         fontWeight = FontWeight.W600,
@@ -80,39 +104,46 @@ fun Verses(chapter: Chapter, verseId: String){
                         textAlign = TextAlign.Center
                     )
 
-                    Spacer(modifier = Modifier.padding(7.dp))
-                    
-                    Row(modifier = Modifier.padding(top = 5.dp, bottom = 5.dp),
+                    Row(
+                        modifier = Modifier.padding(top = 5.dp, bottom = 5.dp),
                         horizontalArrangement = Arrangement.Center,
-                        verticalAlignment = Alignment.CenterVertically) {
-                        if(id == 0){
-                            Card(modifier = Modifier
-                                .size(35.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        if (id == 0) {
+                            Card(
+                                modifier = Modifier
+                                    .size(35.dp),
                                 shape = RoundedCornerShape(corner = CornerSize(35.dp)),
                                 backgroundColor = Color.White,
                                 contentColor = Color.LightGray,
-                                elevation = 5.dp) {
-                                Icon(imageVector = Icons.Default.KeyboardArrowLeft,
-                                    contentDescription = null)
+                                elevation = 5.dp
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowLeft,
+                                    contentDescription = null
+                                )
                             }
-                        }
-                        else{
-                            Card(modifier = Modifier
-                                .size(35.dp)
-                                .clickable(onClick = {
-                                    id -= 1
-                                }),
+                        } else {
+                            Card(
+                                modifier = Modifier
+                                    .size(35.dp)
+                                    .clickable(onClick = {
+                                        id -= 1
+                                    }),
                                 shape = RoundedCornerShape(corner = CornerSize(35.dp)),
                                 backgroundColor = Color.White,
                                 contentColor = Color.Black,
-                                elevation = 5.dp) {
-                                Icon(imageVector = Icons.Default.KeyboardArrowLeft,
-                                    contentDescription = null)
+                                elevation = 5.dp
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowLeft,
+                                    contentDescription = null
+                                )
                             }
                         }
 
                         Text(
-                            text = chapter.chapter_content[id].verse_name,
+                            text = chapter.chapterContent[id].verseName,
                             modifier = Modifier.padding(start = 90.dp, end = 90.dp),
                             color = Color(0xFFFD950E),
                             fontWeight = FontWeight.SemiBold,
@@ -120,40 +151,93 @@ fun Verses(chapter: Chapter, verseId: String){
                             style = MaterialTheme.typography.caption
                         )
 
-                        if(id == chapter.total_verses.toInt()-1){
-                            Card(modifier = Modifier
-                                .size(35.dp),
+                        if (id == chapter.totalVerses.toInt() - 1) {
+                            Card(
+                                modifier = Modifier
+                                    .size(35.dp),
                                 shape = RoundedCornerShape(corner = CornerSize(35.dp)),
                                 backgroundColor = Color.White,
                                 contentColor = Color.LightGray,
-                                elevation = 5.dp) {
-                                Icon(imageVector = Icons.Default.KeyboardArrowRight,
-                                    contentDescription = null)
+                                elevation = 5.dp
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowRight,
+                                    contentDescription = null
+                                )
                             }
-                        }
-                        else{
-                            Card(modifier = Modifier
-                                .size(35.dp)
-                                .clickable(onClick = {
-                                    id += 1
-                                }),
+                        } else {
+                            Card(
+                                modifier = Modifier
+                                    .size(35.dp)
+                                    .clickable(onClick = {
+                                        id += 1
+                                    }),
                                 shape = RoundedCornerShape(corner = CornerSize(35.dp)),
                                 backgroundColor = Color.White,
                                 contentColor = Color.Black,
-                                elevation = 5.dp) {
-                                Icon(imageVector = Icons.Default.KeyboardArrowRight,
-                                    contentDescription = null)
+                                elevation = 5.dp
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.KeyboardArrowRight,
+                                    contentDescription = null
+                                )
                             }
                         }
                     }
 
-                    Spacer(modifier = Modifier.padding(7.dp))
+                    if (isAlreadyFavourite.isNotEmpty()) {
+                        IconButton(
+                            onClick = {
+                                favouriteViewModel.deleteFavourite(
+                                    Favourite(
+                                        chapterId = chapter.chapterId,
+                                        verseId = (id + 1).toString()
+                                    )
+                                )
+                                Toast.makeText(
+                                    context, "Deleted from Favourites",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.Favorite,
+                                contentDescription = "Favourites Icon"
+                            )
+                        }
+                    }
+                    else{
+                        IconButton(
+                            onClick = {
+                                favouriteViewModel.insertFavourite(
+                                    Favourite(
+                                        chapterId = chapter.chapterId,
+                                        verseId = (id + 1).toString()
+                                    )
+                                )
+                                Toast.makeText(
+                                    context, "Added to Favourites",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Outlined.FavoriteBorder,
+                                contentDescription = "Favourites Icon"
+                            )
+                        }
+                    }
 
-                    Column(modifier = Modifier
-                        .verticalScroll(state = ScrollState(0)),
-                        horizontalAlignment = Alignment.CenterHorizontally) {
+                    Spacer(modifier = Modifier.padding(5.dp))
+
+                    Column(
+                        modifier = Modifier
+                            .verticalScroll(state = ScrollState(0)),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
                         Text(
-                            text = chapter.chapter_content[id].verse,
+                            text = chapter.chapterContent[id].verse,
                             fontSize = 18.sp,
                             fontWeight = FontWeight.W600,
                             style = MaterialTheme.typography.caption,
@@ -161,17 +245,15 @@ fun Verses(chapter: Chapter, verseId: String){
                         )
 
                         Text(
-                            text = chapter.chapter_content[id].verse_meaning,
+                            text = chapter.chapterContent[id].verseMeaning,
                             fontSize = 17.sp,
                             fontWeight = FontWeight.W400,
                             style = MaterialTheme.typography.caption,
                             textAlign = TextAlign.Center
                         )
                     }
-
                 }
             }
         }
     }
-
 }
