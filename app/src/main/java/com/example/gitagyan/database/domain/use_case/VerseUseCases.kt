c4adbbbbbdd5513535b457d8c4d375d6201100cd
Vishal Patil <believeinthelimitless@gmail.com>
2023-10-06 19:45:43 +0530

    Deleted database

--- app/src/main/java/com/example/gitagyan/database/domain/use_case/VerseUseCases.kt
+++
@@ -1,8 +0,0 @@
-package com.example.gitagyan.database.domain.use_case
-
-data class VerseUseCases(
-    val getVerses: GetVerses,
-    val deleteVerse: DeleteVerse,
-    val addVerse: AddVerse,
-    val getVerse: GetVerse
-)
--- app/src/main/java/com/example/gitagyan/database/domain/util/OrderType.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gitagyan.database.domain.util
-
-sealed class OrderType{
-    object Ascending : OrderType()
-    object Descending : OrderType()
-}
