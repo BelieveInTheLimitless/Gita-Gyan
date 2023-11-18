f0e918ed3a25e2e42c17559229e238e1ac928aa3
Vishal Patil <believeinthelimitless@gmail.com>
2023-11-18 23:39:37 +0530

    Refactored Language object

--- app/src/main/java/com/example/gitagyan/data/content/Language.kt
+++
@@ -1,5 +0,0 @@
-package com.example.gitagyan.data.content
-
-object Language {
-    var selectedLanguage: String = "English"
-}
---
+++ app/src/main/java/com/example/gitagyan/model/Languages.kt
@@ -0,0 +1,5 @@
+package com.example.gitagyan.model
+
+object Languages{
+    var selectedLanguage: String = "English"
+}
