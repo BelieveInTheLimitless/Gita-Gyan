abd0d1770fe7f945c9c8920f662bfc6bc1eaeaa6
Vishal Patil <believeinthelimitless@gmail.com>
2023-12-15 00:07:21 +0530

    Added UI to access currently reading verse

--- app/src/main/java/com/example/gitagyan/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/gitagyan/screens/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.gitagyan.screens.home
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,7 +16,10 @@ import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
@@ -24,7 +28,9 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.gitagyan.R
 import com.example.gitagyan.data.content.Chapter
 import com.example.gitagyan.model.Languages
 import com.example.gitagyan.data.content.english.getEnglishChapters
@@ -44,9 +50,71 @@ fun HomeScreen(navController: NavController){
 @Composable
 fun MainContent(
     navController: NavController,
-    chapterList: List<Chapter> = if (Languages.selectedLanguage == "English") getEnglishChapters() else getHindiChapters()
+    chapterList: List<Chapter> = if (Languages.selectedLanguage == "English") getEnglishChapters() else getHindiChapters(),
+    currentVerseViewModel: CurrentVerseViewModel = hiltViewModel()
 ){
     Column(modifier = Modifier.padding(10.dp)) {
+        val currentVerseList = currentVerseViewModel.currentVerseList.collectAsState().value
+        if(currentVerseList.isNotEmpty()){
+            Card(modifier = Modifier
+                .padding(3.dp)
+                .fillMaxWidth()
+                .clickable(
+                    onClick = {
+                        navController.navigate(AppScreens.VerseScreen.name + "/${currentVerseList[0].chapterId - 1}" + "/${currentVerseList[0].verseId - 1}" + "/${true}")
+                    }
+                ),
+                shape = RoundedCornerShape(corner = CornerSize(20.dp)),
+                backgroundColor = Color(0xFFFD950E),
+                contentColor = Color.Black,
+                elevation = 7.dp) {
+                Row(
+                ) {
+                    Column(
+                        modifier = Modifier.padding(15.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Image(painter = painterResource(id = R.drawable.krishna_arjuna),
+                            contentDescription = null,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(16.dp))
+                            )
+                    }
+                    Column(
+                        modifier = Modifier.padding(10.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = if (Languages.selectedLanguage == "English") "You were reading..." else "आप पढ़ रहे थे...",
+                            color = Color(0xFFFFFFFF),
+                            fontWeight = FontWeight.SemiBold,
+                            fontSize = 20.sp,
+                            style = MaterialTheme.typography.caption,
+                            textAlign = TextAlign.Center
+                        )
+
+                        Text(
+                            text = chapterList[currentVerseList[0].chapterId - 1].chapter,
+                            color = Color(0xFFFFFFFF),
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            style = MaterialTheme.typography.caption
+                        )
+                        Text(
+                            text = chapterList[currentVerseList[0].chapterId - 1].chapterContent[currentVerseList[0].verseId - 1].verseName,
+                            color = Color(0xFFFFFFFF),
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.W400,
+                            style = MaterialTheme.typography.caption
+                        )
+                    }
+                }
+            }
+        }
         LazyColumn{
             items(items = chapterList){
                 ChapterRow(chapter = it){ chapterId ->
@@ -68,8 +136,8 @@ fun ChapterRow(
 
     Card(modifier = Modifier
         .padding(3.dp)
-        .fillMaxWidth().
-        clickable {
+        .fillMaxWidth()
+        .clickable {
             onItemClick(chapter.chapterId)
         },
         shape = RoundedCornerShape(corner = CornerSize(20.dp)),
