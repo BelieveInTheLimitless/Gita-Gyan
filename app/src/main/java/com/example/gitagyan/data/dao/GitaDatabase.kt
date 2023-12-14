c0e86e869c9b0cf91beae7f3c267b20e6d6c3ff3
Vishal Patil <believeinthelimitless@gmail.com>
2023-12-14 23:55:17 +0530

    Added current verse entity to Database

--- app/src/main/java/com/example/gitagyan/data/dao/GitaDatabase.kt
+++ app/src/main/java/com/example/gitagyan/data/dao/GitaDatabase.kt
@@ -2,10 +2,11 @@ package com.example.gitagyan.data.dao
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.gitagyan.model.CurrentVerse
 import com.example.gitagyan.model.Favourite
 import com.example.gitagyan.model.Language
 
-@Database(entities = [Favourite::class, Language::class], version = 3, exportSchema = false)
+@Database(entities = [Favourite::class, Language::class, CurrentVerse::class], version = 4, exportSchema = false)
 abstract class GitaDatabase: RoomDatabase() {
     abstract fun gitaDao(): GitaDao
 }
