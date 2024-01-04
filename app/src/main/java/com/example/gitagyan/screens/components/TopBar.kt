f4b11cd4cfecb23a8fa3c420f907310381929b0e
Vishal Patil <believeinthelimitless@gmail.com>
2024-01-04 20:03:49 +0530

    Created new top bar

---
+++ app/src/main/java/com/example/gitagyan/screens/components/TopBar.kt
@@ -0,0 +1,90 @@
+package com.example.gitagyan.screens.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.gitagyan.model.Languages
+import com.example.gitagyan.screens.profile.LanguageViewModel
+
+@Composable
+fun TopBar(isHomeScreen: Boolean = false, languageViewModel: LanguageViewModel = hiltViewModel(), onClicked: ()-> Unit){
+
+    val language = languageViewModel.languageList.collectAsState().value
+
+    if (language.isNotEmpty()){
+        Languages.selectedLanguage = language[0].language
+    }
+
+    TopAppBar(backgroundColor = Color.White) {
+        val appName =
+            if (Languages.selectedLanguage == "English") "Gita Gyan" else "गीता ज्ञान"
+        if (isHomeScreen) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = appName,
+                    color = Color.Black,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Serif,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.caption
+                )
+            }
+        }
+        else {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(
+                    onClick = {
+                        onClicked()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back Arrow"
+                    )
+                }
+                Text(
+                    text = appName,
+                    color = Color.Black,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Serif,
+                    style = MaterialTheme.typography.caption
+                )
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = "Settings",
+                        tint = Color.Transparent
+                    )
+                }
+            }
+        }
+    }
+}
