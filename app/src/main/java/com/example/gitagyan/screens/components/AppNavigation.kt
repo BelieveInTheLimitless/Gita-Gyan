1b65d1b3a4e974a7bb23e7fb51c340cb9f279e59
Vishal Patil <believeinthelimitless@gmail.com>
2024-01-04 20:08:58 +0530

    Created new App Navigation System

---
+++ app/src/main/java/com/example/gitagyan/screens/components/AppNavigation.kt
@@ -0,0 +1,74 @@
+package com.example.gitagyan.screens.components
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.gitagyan.navigation.AppScreens
+import com.example.gitagyan.screens.favourite.FavouriteNavHost
+import com.example.gitagyan.screens.home.HomeNavHost
+import com.example.gitagyan.screens.profile.ProfileNavHost
+import com.example.gitagyan.screens.search.SearchNavHost
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun AppNavigation(){
+
+    val rootNavController = rememberNavController()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            BottomBar(rootNavController = rootNavController)
+        },
+        containerColor = Color(0xFFFD950E)
+    ) {
+        NavHost(navController = rootNavController,
+            startDestination = AppScreens.HomeScreen.name,
+            modifier = Modifier.padding(it),
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            popEnterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                    animationSpec = tween(700)
+                )
+            },
+            popExitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                    animationSpec = tween(700)
+                )
+            }) {
+            composable(AppScreens.HomeScreen.name) {
+                HomeNavHost()
+            }
+            composable(AppScreens.SearchScreen.name) {
+                SearchNavHost(rootNavController = rootNavController)
+            }
+            composable(AppScreens.FavouriteScreen.name) {
+                FavouriteNavHost(rootNavController = rootNavController)
+            }
+            composable(AppScreens.ProfileScreen.name) {
+                ProfileNavHost(rootNavController = rootNavController)
+            }
+        }
+    }
+}
